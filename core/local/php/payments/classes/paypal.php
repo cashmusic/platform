1fbef71708e847450fd0ea057ce7b70fb9c1cd6b
Jesse von Doom <jesse@cashmusic.org>
2010-08-11 12:16:50 -0400

    added Paypal class (needs debugging) / I love Dropbox

---
+++ core/local/php/payments/classes/paypal.php
@@ -0,0 +1,230 @@
+<?php
+/**
+ * The PaypalSeed class sets up cURL and speaks to the Paypal NVP API.
+ *
+ * @package seed.org.cashmusic
+ * @author Jesse von Doom / CASH Music
+ * @link http://cashmuisc.org/
+ **/
+class PaypalSeed {
+	protected $api_username, $api_password, $api_signature, $api_endpoint, $api_version, $paypal_base_url, $error_message, $token;
+
+	public function __construct($username, $password, $signature, $environment='sandbox', $token_=false) {
+		// Set up your API credentials, PayPal end point, and API version.
+		$this->api_username = urlencode($username);
+		$this->api_password = urlencode($password);
+		$this->api_signature = urlencode($signature);
+		$this->api_version = urlencode('63.0');
+		$this->error_message = 'No error.';
+		
+		// Check for GET/POST token, else set false or manually-set at start
+		if (isset($_GET['token'])) {
+			$this->token = urldecode($_GET['token']);
+		} else {
+			$this->token = $token_;
+		}
+		
+		// Environments: 'sadbox' / 'beta-sandbox' / 'live'
+		$this->api_endpoint = "https://api-3t.paypal.com/nvp";
+		$this->paypal_base_url = "https://www.paypal.com/webscr&cmd=";
+		if("sandbox" === $environment || "beta-sandbox" === $environment) {
+			$this->api_endpoint = "https://api-3t.$environment.paypal.com/nvp";
+			$this->paypal_base_url = "https://www.$environment.paypal.com/webscr&cmd=";
+		}
+	}
+
+	protected function prepCURL() {
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $this->api_endpoint);
+		curl_setopt($ch, CURLOPT_VERBOSE, 1);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_POST, 1);
+		return $ch;
+	}
+	
+	protected function postToPaypal($methodName_, $nvp_str_) {
+		// Set the API operation, version, and API signature in the request.
+		$nvpreq = "METHOD=$methodName_&VERSION=" . $this->api_version;
+		$nvpreq .= "&PWD=" . $this->api_password;
+		$nvpreq .= "&USER=" . $this->api_username;
+		$nvpreq .= "&SIGNATURE=" . $this->api_signature;
+		$nvpreq .= $nvp_str_;
+
+		// Set the request as a POST FIELD for curl.
+		$curlobj = $this->prepCURL();
+		curl_setopt($curlobj, CURLOPT_POSTFIELDS, $nvpreq);
+
+		// Get response from the server.
+		$httpResponse = curl_exec($curlobj);
+
+		if(!$httpResponse) {
+			exit('$methodName_ failed: '.curl_error($curlobj).'('.curl_errno($curlobj).')');
+		}
+
+		// Extract the response details.
+		$http_response = explode("&", $httpResponse);
+		$parsed_response = array();
+		foreach ($http_response as $i => $value) {
+			$tmpAr = explode("=", $value);
+			if(sizeof($tmpAr) > 1) {
+				$parsed_response[$tmpAr[0]] = $tmpAr[1];
+			}
+		}
+
+		if((0 == sizeof($parsed_response)) || !array_key_exists('ACK', $parsed_response)) {
+			$this->setErrorMessage("Invalid HTTP Response for POST (" . $nvpreq . ") to " . $this->api_endpoint);
+			return false;
+		}
+
+		if("SUCCESS" == strtoupper($parsed_response["ACK"]) || "SUCCESSWITHWARNING" == strtoupper($parsed_response["ACK"])) {
+			return $parsed_response;
+		} else {
+			$this->setErrorMessage(print_r($parsed_response, true));
+			return false;
+		}
+	}
+	
+	protected function setErrorMessage($msg) {
+		$this->error_message = $msg;
+	}
+	
+	public function getErrorMessage() {
+		return $this->error_message;
+	}
+	
+	public function setExpressCheckout(
+		$payment_amount,
+		$description,
+		$return_url,
+		$cancel_url,
+		$request_shipping_info=true,
+		$allow_cc=true,
+		$allow_note=true,
+		$invoice=false,
+		$currency_id='USD', /* 'USD', 'GBP', 'EUR', 'JPY', 'CAD', 'AUD' */
+		$payment_type='Sale', /* 'Sale', 'Order', or 'Authorization' */
+		$hdr_img=false, /* use https:// location to avoid errors */
+		$hdr_img_bordercolor='FFFFFF',
+		$hdr_bgcolor='FFFFFF',
+		$page_bgcolor='FFFFFF'
+	) {
+		// Set NVP variables:
+		$nvp_str = "&PAYMENTREQUEST_0_AMT=" . urlencode($payment_amount);
+		$nvp_str .= "&PAYMENTREQUEST_0_PAYMENTACTION=" . urlencode($payment_type);
+		$nvp_str .= "&PAYMENTREQUEST_0_CURRENCYCODE=" . urlencode($currency_id);
+		$nvp_str .= "&RETURNURL=" . urlencode($return_url);
+		$nvp_str .= "&CANCELURL=" . urlencode($cancel_url);
+		$nvp_str .= "&PAYMENTREQUEST_0_DESC=" . urlencode($description);
+		if ($request_shipping_info) {
+			$nvp_str .= "&NOSHIPPING=0";
+		} else {
+			$nvp_str .= "&NOSHIPPING=1";
+		}
+		if ($allow_note) {
+			$nvp_str .= "&ALLOWNOTE=1";
+		} else {
+			$nvp_str .= "&ALLOWNOTE=0";
+		}
+		if ($invoice) {
+			$nvp_str .= "&PAYMENTREQUEST_0_INVNUM=" . urlencode($invoice);
+		}
+		if ($allow_cc) {
+			$nvp_str .= "&SOLUTIONTYPE=Sole";
+			$nvp_str .= "&LANDINGPAGE=Billing";
+		}
+		if ($invoice) {
+			$hdr_img .= "&HDRIMG=" . urlencode($hdr_img);
+		}
+		$nvp_str .= "&HDRBORDERCOLOR=" . $hdr_img_bordercolor;
+		$nvp_str .= "&HDRBACKCOLOR=" . $hdr_bgcolor;
+		$nvp_str .= "&PAYFLOWCOLOR=" . $page_bgcolor;
+		
+		$parsed_response = $this->postToPaypal('SetExpressCheckout', $nvp_str);
+		if (!$parsed_response) {
+			$this->setErrorMessage('SetExpressCheckout failed: ' . $this->getErrorMessage());
+			return false;
+		} else {
+			// Redirect to paypal.com.
+			$token = urldecode($parsed_response["TOKEN"]);
+			$payPalURL = $this->paypal_base_url . "_express-checkout&token=$token";
+			header("Location: $payPalURL");
+			exit;
+		}
+	}
+	
+	public function getExpressCheckout() {
+		if ($this->token) {
+			$nvp_str = "&TOKEN=" . $this->token;
+			$parsed_response = $this->postToPaypal('GetExpressCheckoutDetails', $nvp_str);
+			if (!$parsed_response) {
+				$this->setErrorMessage('GetExpressCheckoutDetails failed: ' . $this->getErrorMessage());
+				return false;
+			}
+		} else {
+			$this->setErrorMessage("No token was found.");
+			return false;
+		}
+	}
+	
+	public function DoExpressCheckout($payment_amount,$currency_id='USD',$payment_type='Sale') {
+		if ($this->token) {
+			$token_details = $this->getExpressCheckout();
+			$payer_id = $token_details['PAYERID'];
+			$nvp_str = "&TOKEN=" . $this->token;
+			$nvp_str .= "&PAYERID=" . $payer_id;
+			$nvp_str .= "&PAYMENTREQUEST_0_PAYMENTACTION=" . $payment_type;
+			$nvp_str .= "&PAYMENTREQUEST_0_AMT=" . urlencode($payment_amount);
+			$nvp_str .= "&PAYMENTREQUEST_0_CURRENCYCODE=" . $currency_id;
+			
+			$parsed_response = $this->postToPaypal('DoExpressCheckoutPayment', $nvp_str);
+			
+			if (!$parsed_response) {
+				$this->setErrorMessage('DoExpressCheckout failed: ' . $this->getErrorMessage());
+				return false;
+			}
+		} else {
+			$this->setErrorMessage("No token was found.");
+			return false;
+		}
+	}
+	
+	public function DoRefund($transaction_id, $refund_amount, $fullrefund=true,$memo=false,$currency_id='USD') {
+		if ($fullrefund) {
+			$refund_type = "Full";
+		} else {
+			$refund_type = "Partial";
+		}
+		
+		$nvp_str = "&TRANSACTIONID=" . $transaction_id;
+		$nvp_str .= "&REFUNDTYPE=" . $refund_type;
+		$nvp_str .= "&CURRENCYCODE=" . $currency_id;
+
+		if($memo) {
+			$nvp_str .= "&NOTE=" . $memo;
+		}
+		
+		if (!$fullrefund) {
+			if(!isset($refund_amount)) {
+				$this->setErrorMessage('Partial Refund: must specify amount.');
+				return false;
+			} else {
+		 		$nvp_str .= "&AMT=" . $refund_amount;
+			}
+
+			if(!$memo) {
+				$this->setErrorMessage('Partial Refund: must specify memo.');
+				return false;
+			}
+		}
+		
+		$parsed_response = $this->postToPaypal('RefundTransaction', $nvp_str);
+
+		if (!$parsed_response) {
+			$this->setErrorMessage('RefundTransaction failed: ' . $this->getErrorMessage());
+			return false;
+		}
+	}
+} // END class 
+?>
---
+++ core/local/php/payments/index.php
@@ -0,0 +1,109 @@
+<?php
+include('classes/paypal.php');
+$ppobj = new PaypalSeed('sell_1275529145_biz_api1.cashmusic.org','1275529151','AFcWxV21C7fd0v3bYYYRCpSSRl31AGCG62tWdLmw5MVRVpwXFOJVoCjk');
+if (!$ppobj->SetExpressCheckout(5,'stuff','http://localhost:8888/payments/','http://localhost:8888/payments/',false,true,true,false,'USD','Sale',false,'000000','000000','000000')) {
+	echo $ppobj->getErrorMessage();
+}
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<title>Showcase / CASH Music</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="alternate" type="application/rss+xml" title="CASH Music blog RSS Feed" href="http://feeds.feedburner.com/cashmusic" /> 
+<meta name="description" content="CASH Music is a nonprofit music tech foundation." />
+<meta name="keywords" content="artists art copyleft open community free music musicians code download alternative" />
+
+<link href="/assets/css/main.css" rel="stylesheet" type="text/css" />
+<link rel="icon" type="image/png" href="/assets/images/icons/cash.png" />
+
+<!--[if lt IE 7]>
+        <script type="text/javascript" src="/assets/scripts/unitpngfix.js"></script>
+<![endif]-->
+</head>
+
+<body>
+
+<div id="wrap">
+
+	<div id="cashstatementspc">
+		<div id="cash_sitelogo"><a href="/"><img src="/assets/images/cash.png" alt="CASH Music" width="30" height="30" /></a></div>
+		<div id="mainmenu">
+			<a href="/about/">About</a>
+			<a href="http://blog.cashmusic.org/">Blog</a>
+			<a href="/showcase/">Showcase</a>
+			<a href="/donate/">Give Support</a>
+			<a href="/tools/">Download</a>
+			<a href="http://help.cashmusic.org/">Get Help</a>
+		</div>
+
+		<div id="cashstatement">
+			<h1>Test</h1>
+			<p>
+				
+			</p>
+		</div>
+	</div>
+	
+	<div id="mainspc">
+		
+	</div>
+</div>
+
+<div id="footer"> 
+	<div id="mainfooter">
+		<h2>CASH Music</h2>
+		<div id="footercontactspc" class="toprow">
+			<p>
+			<b>Contact Us</b><br />
+			<a href="mailto:info@cashmusic.org">info@cashmusic.org</a>
+			<br /><br />	
+			CASH Music<br />
+			PO Box 3782<br /> 
+			Newport, RI<br />
+			02840
+			</p>
+		</div>
+		<div id="footerdontatespc" class="toprow">
+			<p>
+			<b>Please Consider Giving</b><br />
+			Donations are greatly appreciated and directly support development.  
+			</p>
+			<form method="post" action="http://cashmusic.org/beginpayment.php">
+			<input type="text" name="a" value="5.00" style="width:5em;" /> 
+			<input type="hidden" name="w" value="donation" />
+			<input type="submit" value="contribute" class="button" /><br />
+			<img src="/assets/images/paypal_logo_w.png" width="38" height="12" alt="paypal" />
+			</form>
+			<p class="darker">
+			CASH Music is a registered non-profit in Rhode Island, and has pending 501(c)3 tax-exempt status.
+			</p>
+		</div>
+		
+		
+		<div>
+			<p><b>Visit Us Elsewhere</b><br /><br /></p>
+			<div id="socialtwitter" class="sociallink"><a href="http://twitter.com/cashmusic"><img src="/assets/images/clear.gif" alt="twitter" /></a></div>
+			<div id="socialfacebook" class="sociallink"><a href="http://facebook.com/cashmusic.org"><img src="/assets/images/clear.gif" alt="facebook" /></a></div>
+			<div id="socialgithub" class="sociallink"><a href="http://github.com/cashmusic"><img src="/assets/images/clear.gif" alt="github" /></a></div>
+			<div id="sociallighthouse" class="sociallink"><a href="http://cashmusic.lighthouseapp.com/"><img src="/assets/images/clear.gif" alt="lighthouse" /></a></div>
+		</div>
+	</div>
+	
+	<p id="copyrightspc" class="darker"><b>&copy; 2010 CASH Music.</b> Words and images released under a <a href="http://creativecommons.org/licenses/by/3.0/" class="darker">Creative Commons BY license</a>. All our code is open-source. <a href="/about/faq/">Learn more</a>.</p>
+	
+	<a href="/"><img src="/assets/images/cash404040.png" alt="CASH Music" width="30" height="30" id="footerlogo" /></a>
+</div>
+
+<script type="text/javascript">
+//<![CDATA[ 
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+//]]> 
+</script><script type="text/javascript">
+//<![CDATA[ 
+try {var pageTracker = _gat._getTracker("UA-7451645-1");pageTracker._trackPageview();} catch(err) {}
+//]]> 
+</script>
+</body>
+
+</html>
