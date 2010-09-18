8290396b019f344f9d261356abc4b240dfdb1ff8
Jesse von Doom <jesse@cashmusic.org>
2010-09-18 00:41:29 -0700

    Better reporting of states, displaying different page content based on state.

--- core/local/php/payments/index.php
+++ core/local/php/payments/index.php
@@ -42,14 +42,45 @@
 	</div>
 	
 	<div id="mainspc">
-		<a href="./?paypalseed=go">Begin Test</a><br /><br />
-		<pre>
-			<?php
-			echo "<br />CURRENT STATE:<br />".$_SESSION['seed_state']."<br /><br />";
-			echo "<br /><br />RESPONSE:<br />";
-			var_dump($_SESSION['seed_response']);
-			?>
-		</pre>
+		<?php
+		session_start();
+		switch ($_SESSION['seed_state']) {
+			case 'completed':
+				echo(
+					'<b>Transaction details (Save to DB, list, etc.)</b><br />' .
+					'timestamp: ' . urldecode($_SESSION['seed_response']['TIMESTAMP']) . '<br />' .
+					'email: ' . urldecode($_SESSION['seed_response']['EMAIL']) . '<br />' .
+					'payer id: ' . urldecode($_SESSION['seed_response']['PAYERID']) . '<br />' .
+					'first name: ' . urldecode($_SESSION['seed_response']['FIRSTNAME']) . '<br />' .
+					'last name: ' . urldecode($_SESSION['seed_response']['LASTNAME']) . '<br />' .
+					'country: ' . urldecode($_SESSION['seed_response']['COUNTRYCODE']) . '<br />' .
+					'currency: ' . urldecode($_SESSION['seed_response']['PAYMENTREQUEST_0_CURRENCYCODE']) . '<br />' .
+					'amount: ' . urldecode($_SESSION['seed_response']['PAYMENTREQUEST_0_AMT']) . '<br />' .
+					'transaction id: ' . urldecode($_SESSION['seed_response']['PAYMENTREQUEST_0_TRANSACTIONID']) . '<br /><br />' .
+					'full response, JSON-encoded:<br />' . str_replace(',',', ',json_encode($_SESSION['seed_response']))
+				);
+				// once we're done flush session data
+				$_SESSION = array();
+				session_destroy();
+				session_write_close();
+		        break;
+		    case 'failed':
+				echo '<p>Sorry, the payment failed. Please check your login and payment source and <a href="./?seed_begin=go">try again</a>.</p>';
+		        break;
+		    case 'uncompleted':
+				echo '<p>Sorry, your transaction could not be completed. Please <a href="./?seed_begin=go">try again</a>.</p>';
+				break;
+			default:
+				?>
+				<p>
+					This link will initiate a $5 charge for 'stuff' to the Paypal sandbox server:
+				</p>
+				<a href="./?seed_begin=go">Begin Test</a><br />
+				<small>$5 charge</small>
+				
+				<?php
+		}
+		?>
 	</div>
 </div>
 
--- core/local/php/payments/logic/paypal.php
+++ core/local/php/payments/logic/paypal.php
@@ -17,6 +17,7 @@
  **/
 
 // define paypal credentials
+// NEED TO DEFINE REGULAR AND MICROPAYMENT ADDRESSES/KEYS!
 define('PAYPAL_ADDRESS', 'sell_1275529145_biz_api1.cashmusic.org');
 define('PAYPAL_KEY', '1275529151');
 define('PAYPAL_SECRET', 'AFcWxV21C7fd0v3bYYYRCpSSRl31AGCG62tWdLmw5MVRVpwXFOJVoCjk');
@@ -27,7 +28,10 @@ if (!isset($_SESSION['seed_state'])) $_SESSION['seed_state'] = 'before';
 if (!isset($_SESSION['seed_response'])) $_SESSION['seed_response'] = false;
 if (!isset($_SESSION['seed_error'])) $_SESSION['seed_error'] = false;
 
-if ($_GET['paypalseed'] == 'go') {
+// page URL
+$urlMinusGet = 'http'.((empty($_SERVER['HTTPS'])&&$_SERVER['SERVER_PORT']!=443)?'':'s').'://'.$_SERVER['HTTP_HOST'].strtok($_SERVER['REQUEST_URI'],'?');
+
+if ($_GET['seed_begin'] == 'go') {
 	// reset all session variables, just in case
 	$_SESSION['seed_state'] = 'before';
 	$_SESSION['seed_response'] = false;
@@ -35,20 +39,30 @@ if ($_GET['paypalseed'] == 'go') {
 	
 	include(__DIR__.'/../classes/paypal.php');
 	$seed_ppobj = new PaypalSeed(PAYPAL_ADDRESS,PAYPAL_KEY,PAYPAL_SECRET);
-	if (!$seed_ppobj->SetExpressCheckout(5,'stuff','the finest stuff in the world','http://cashmusic.org/tools/_payments/','http://cashmusic.org/tools/_payments/',false,true,false,false,'USD','Sale',false,'000000','000000','000000')) {
+	if (!$seed_ppobj->SetExpressCheckout(5,'stuff','the finest stuff in the world',$urlMinusGet,$urlMinusGet,false,true,false,false,'USD','Sale',false,'000000','000000','000000')) {
 		$_SESSION['seed_state'] = 'error';
 		$_SESSION['seed_error'] = $seed_ppobj->getErrorMessage();
 	}
 } else if (isset($_GET['token']) && isset($_GET['PayerID'])) {
 	// data returned from Paypal
-	$_SESSION['seed_state'] = 'after';
 	include(__DIR__.'/../classes/paypal.php');
 	$seed_ppobj = new PaypalSeed(PAYPAL_ADDRESS,PAYPAL_KEY,PAYPAL_SECRET);
 	$seed_ppobj->doExpressCheckout();
 	$_SESSION['seed_response'] = $seed_ppobj->getExpressCheckout();
+	
 	// handle all processing then redirect to self, cleaning the URL
-	$urlMinusGet = 'http'.((empty($_SERVER['HTTPS'])&&$_SERVER['SERVER_PORT']!=443)?'':'s').'://'.$_SERVER['HTTP_HOST'].strtok($_SERVER['REQUEST_URI'],'?');
+	switch ($_SESSION['seed_response']['CHECKOUTSTATUS']) {
+	    case 'PaymentActionCompleted':
+			$_SESSION['seed_state'] = 'completed';
+	        break;
+	    case 'PaymentActionFailed':
+			$_SESSION['seed_state'] = 'failed';
+	        break;
+	    default:
+	       $_SESSION['seed_state'] = 'uncompleted';
+	}
 	header("Location: $urlMinusGet");
+	exit;
 } else if (isset($_GET['token']) && !isset($_GET['PayerID'])) {
 	// cancellation return from Paypal
 	$_SESSION['seed_state'] = 'cancelled';
