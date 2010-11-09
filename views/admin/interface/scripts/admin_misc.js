6bea02eab34ba322c1e331c24f71d0c0b2426cdb
Jesse von Doom <jesse@cashmusic.org>
2010-11-09 10:32:58 -0800

    removed old admin sketch code

--- views/admin/interface/scripts/admin_misc.js
+++
@@ -1,7 +0,0 @@
-window.addEvent('domready', function() {
-    $$('div.navitem').each(function(item){
-    	item.addEvent('click', function(e) {
-    		window.location.pathname = item.getElement('a').getProperty('href');
-    	});
-	});
-});
