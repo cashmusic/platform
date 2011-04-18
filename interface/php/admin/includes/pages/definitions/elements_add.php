bdb5c878efc2ad216b1024b5f8fd8756f51882ad
Jesse von Doom <jesse@cashmusic.org>
2011-04-17 21:44:07 -0700

    new controller structure allows for request parameters in the URL

--- interface/php/admin/includes/pages/definitions/elements_add.php
+++ interface/php/admin/includes/pages/definitions/elements_add.php
@@ -1,5 +1,5 @@
 <?php
 // add unique page settings:
-$page_title = 'Elements: Email For Downlaod';
+$page_title = 'Elements: Add Elements';
 $page_tips = 'They give an email, you give a download.';
 ?>
