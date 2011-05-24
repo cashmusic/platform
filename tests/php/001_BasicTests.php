7c5a968d6c4f835f292ad6f2b93bc0edba9657e3
Jonathan "Duke" Leto <jonathan@leto.net>
2011-05-24 11:53:59 -0700

    Add some more error reporting

--- tests/php/001_BasicTests.php
+++ tests/php/001_BasicTests.php
@@ -1,4 +1,8 @@
 <?php
+# error reporting
+ini_set('display_errors',1);
+error_reporting(E_ALL|E_STRICT);
+
 require_once 'PHPUnit/Autoload.php';
 require_once dirname(__FILE__).'/../../core/php/cashmusic.php';
 
