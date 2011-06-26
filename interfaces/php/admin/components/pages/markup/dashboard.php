16e8c954ef88748e3105322aa163e2fad02f101e
Jesse von Doom <jesse@cashmusic.org>
2011-06-26 14:32:07 -0700

    starting work on dashboard mocks, more interface

--- interfaces/php/admin/components/pages/markup/dashboard.php
+++ interfaces/php/admin/components/pages/markup/dashboard.php
@@ -1,3 +1,49 @@
-<p>
-	In progress.
-</p>
+<div class="col_onethird">
+	<h2>Welcome!</h2>
+	<p>
+	This is the default landing page. We'll keep this corner for news, updates, and system information.
+	</p><p> 
+	The other sections of the page will focus on high-level data showing element usage, traffic, and
+	relevant statistics. Imagine it looks awesome now...it will when we release.
+	</p>
+</div><div class="col_twothirds lastcol">
+	<h2 class="usecolor1">Elements</h2>
+	...
+</div>
+	
+<div class="clearfix tall">.</div>
+
+<div class="col_onefourth">
+	<h2 class="usecolor2">Assets</h2>
+	<b>Most Accessed</b>
+	<ol>
+		<li>...</li>
+		<li>...</li>
+		<li>...</li>
+	</ol>
+	<b>Recently Added</b>
+	<ul class="nobullets">
+		<li>...</li>
+		<li>...</li>
+		<li>...</li>
+	</ul>
+</div><div class="col_onefourth">
+	<h2 class="usecolor3">People</h2>
+	<p>
+		<b>List Name</b><br />
+		Total members: 4321<br />
+		Last 7 days: 123<br />
+		view details
+	</p><p>
+		<b>List Name</b><br />
+		Total members: 4321<br />
+		Last 7 days: 123<br />
+		view details
+	</p>
+</div><div class="col_onefourth">
+	<h2 class="usecolor4">Commerce</h2>
+	...
+</div><div class="col_onefourth lastcol">
+	<h2 class="usecolor5">Calendar</h2>
+	...
+</div>
