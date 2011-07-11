e96824e53b21ee0f6ebcd25712acc5cc4c80caa4
Jesse von Doom <jesse@cashmusic.org>
2011-07-10 22:52:59 -0700

    simplified the JS load on admin pages, finished menus in the chrome, new page tip style

--- interfaces/php/admin/ui/default/assets/scripts/admin_misc.js
+++ interfaces/php/admin/ui/default/assets/scripts/admin_misc.js
@@ -1,7 +1,17 @@
 window.addEvent('domready', function() {
-    $$('div.navitem').each(function(item){
-    	item.addEvent('click', function(e) {
-    		window.location = item.getElement('a').getProperty('href');
-    	});
+	$$('div.navitem').each(function(item){
+		item.addEvent('click', function(e) {
+			window.location = item.getElement('a').getProperty('href');
+		});
+	});
+	
+	document.id('pagetips').fade('hide');
+	document.id('tipslink').addEvent('click', function(e) {
+		e.stop();
+		document.id('pagetips').fade('in');
+	});
+	document.id('tipscloselink').addEvent('click', function(e) {
+		e.stop();
+		document.id('pagetips').fade('out');
 	});
 });
