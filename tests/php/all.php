993d9a4670d86b5a003b996c6385810291f70d02
Jonathan "Duke" Leto <jonathan@leto.net>
2011-07-15 14:46:26 -0700

    Add a basic test for CASHDBA objects

--- tests/php/all.php
+++ tests/php/all.php
@@ -4,8 +4,10 @@ require_once('tests/lib/simpletest/autorun.php');
 class AllTests extends TestSuite {
     function AllTests() {
         $this->TestSuite('All tests');
+        // this sucks. We should auto-add anything in tests/php/*.php
         $this->addFile('tests/php/001_BasicTests.php');
         $this->addFile('tests/php/002_CashData.php');
+        $this->addFile('tests/php/003_CASHDBA.php');
     }
 }
 ?>
