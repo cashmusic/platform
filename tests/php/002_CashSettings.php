ddb3cdc5ab081cfdbad85e129cb81e2b10f1461d
Jonathan "Duke" Leto <jonathan@leto.net>
2011-07-17 00:31:44 -0700

    Make the tests pass

---
+++ tests/php/002_CashSettings.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once('tests/lib/simpletest/autorun.php');
+require_once('core/php/cashmusic.php');
+
+class CashDataTests extends UnitTestCase {
+
+    function testCASHSettings(){
+        $data = new CASHSettings();
+    }
+
+}
+?>
