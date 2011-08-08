0cd03dbd8e7cfd0802fb53616f56113c52520aa3
Jonathan "Duke" Leto <jonathan@leto.net>
2011-08-07 22:03:55 -0700

    Syntax check the main cashmusic.php file

--- tests/php/000_syntax.php
+++ tests/php/000_syntax.php
@@ -4,7 +4,7 @@ require_once('tests/lib/simpletest/autorun.php');
 
 class SyntaxTests extends UnitTestCase {
     public function testFrameworkSyntax() {
-        $test_files = glob("{framework/php/*/*.php,framework/php/classes/*/*.php}", GLOB_BRACE);
+        $test_files = glob("{framework/php/cashmusic.php,framework/php/*/*.php,framework/php/classes/*/*.php}", GLOB_BRACE);
         foreach ($test_files as $file){
             system("php -l $file", $code);
             $this->assertTrue($code == 0);
