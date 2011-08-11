195fdefe84f5197a5a523e6016cbd40163ec0a2b
Jonathan "Duke" Leto <jonathan@leto.net>
2011-08-10 21:59:23 -0700

    Create a single include for tests, so we can modify the basic test infrastructure without needing to modify all the tests

---
+++ tests/php/base.php
@@ -0,0 +1,12 @@
+<?php
+
+// All tests should include this file, so we can modify basic test functionality
+// here without needing to modify all our tests
+
+// this includes the basic SimpleTest library
+require_once('tests/lib/simpletest/autorun.php');
+
+// this load CASH Music DIY
+require_once('framework/php/cashmusic.php');
+
+?>
