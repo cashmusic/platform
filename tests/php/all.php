a86dae920ad3e53bae0d8502af3640673ce90fa0
Jonathan "Duke" Leto <jonathan@leto.net>
2011-08-13 08:22:09 -0700

    Tweak tests/php/all.php

--- tests/php/all.php
+++ tests/php/all.php
@@ -1,10 +1,11 @@
 <?php
-require_once('tests/lib/simpletest/autorun.php');
+require_once('tests/php/base.php');
 
 class AllTests extends TestSuite {
     function AllTests() {
         $this->TestSuite('All tests');
         // All tests should be of the form NNN_description.php
+        // Notably, this excludes all.php and base.php, which are special
         $test_files = glob("tests/php/*_*.php");
         foreach ($test_files as $test) {
             $this->addFile($test);
