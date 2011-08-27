765684b1d108c64efde3deb944ca53136f1d7418
Jesse von Doom <jesse@cashmusic.org>
2011-08-26 21:44:43 -0700

    better docs/dev site added. once the dev installer is updated the repo will serve as a complete local site with config script to get it up and going. much more dev friendly.

--- interfaces/php/admin/constants.php
+++ interfaces/php/admin/constants.php
@@ -3,6 +3,6 @@ $root = dirname(__FILE__);
 $cashmusic_root = $root . "/../../../framework/php/cashmusic.php";
 
 define('ADMIN_BASE_PATH', $root);
-define('ADMIN_WWW_BASE_PATH', '/admin');
+define('ADMIN_WWW_BASE_PATH', '/interfaces/php/admin');
 define('CASH_PLATFORM_PATH', $cashmusic_root);
 ?>
