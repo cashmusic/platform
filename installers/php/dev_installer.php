50585900ea5146280c42184641e9924db2f55376
Jesse von Doom <jesse@cashmusic.org>
2011-09-01 22:06:50 -0700

    sqlite support in the dev installer, working with CASHDBA -- sqlite3 via PDO - Fixes #2

--- installers/php/dev_installer.php
+++ installers/php/dev_installer.php
@@ -1,6 +1,25 @@
 <?php
-// Courtesy of http://us3.php.net/manual/en/features.commandline.io-streams.php#101307
+/**
+ * CASH Music Dev Installer
+ *
+ * Takes the CASH DIY repo and sets it up  as a working platform instance that
+ * can be directly coded against. Command-line script.
+ *
+ * USAGE:
+ * php installers/php/dev_installer.php 
+ * follow prompts. 
+ * 
+ *
+ * @package diy.org.cashmusic
+ * @author CASH Music
+ * @link http://cashmusic.org/
+ *
+ * Copyright (c) 2011, CASH Music
+ * Licensed under the Affero General Public License version 3.
+ * See http://www.gnu.org/licenses/agpl-3.0.html
+*/
 function readStdin($prompt, $valid_inputs = false, $default = '') {
+	// Courtesy of http://us3.php.net/manual/en/features.commandline.io-streams.php#101307
 	while(!isset($input) || (is_array($valid_inputs) && !in_array(strtolower($input), $valid_inputs))) {
 		echo $prompt;
 		$input = strtolower(trim(fgets(STDIN)));
@@ -66,9 +85,10 @@ if(!defined('STDIN')) { // force CLI, the browser is *so* 2007...
 			$data = array(
 				'email_address' => $user_email,
 				'password'      => $password_hash,
+				'is_admin'      => true,
 				'creation_date' => time()
 			);
-			$query = "INSERT INTO user_users (email_address,password,creation_date) VALUES (:email_address,:password,:creation_date)";
+			$query = "INSERT INTO user_users (email_address,password,is_admin,creation_date) VALUES (:email_address,:password,:is_admin,:creation_date)";
 
 			try {  
 				$q = $pdo->prepare($query);
@@ -83,54 +103,72 @@ if(!defined('STDIN')) { // force CLI, the browser is *so* 2007...
 				die();
 				break;
 			}
-			
-			if ($success) {
-				echo "\nSUCCESS!\nYou'll still need to edit /framework/php/settings/cashmusic.ini.php\n\nOnce done, login using:\n\nemail: $user_email\npassword: $user_password\n\n";
-			}
 		} else {
 			echo "\nOh. Shit. Something's wrong. Couldn't create database tables.\n\n";
 			die();
 			break;
 		}
 	} else if ($db_engine == "sqlite") {
-		// repeated here from the Makefile so we don't require make to do a dev install
-		$cmd = "sqlite3 " . dirname(__FILE__) . "'/../../framework/php/db/cashmusic.db < " . dirname(__FILE__) . "'/../../framework/php/settings/sql/cashmusic_db_sqlite.sql";
+		$installer_root = dirname(__FILE__);
 
-		// if the db already exists, it will warn but continue
-		system($cmd, $code);
+		// if the file exists already, rename it as a backup
+		if (file_exists($installer_root . '/../../framework/php/db/cashmusic.db')) {
+			rename($installer_root . '/../../framework/php/db/cashmusic.db',$installer_root . '/../../framework/php/db/cashmusic.db.bak');
+		} else {
+			// if the directory was never created then create it now
+			if (!file_exists($installer_root . '/../../framework/php/db')) {
+				mkdir($installer_root . '/../../framework/php/db');
+			}
+		}
+		
+		// connect to the new db...will create if not found
 		try {
-			$pdo = new PDO ("sqlite:" . dirname(__FILE__) . "'/../../framework/php/framework/php/db/cashmusic.db");
+			$pdo = new PDO ('sqlite:' . $installer_root . '/../../framework/php/db/cashmusic.db');
+			$pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 		} catch (PDOException $e) {
 			echo "\nOh. Shit. Something's wrong: Couldn't connect to the database. $e\n\n";
 			die();
 			break;
 		}
+		
+		// push in all the tables
+		try {
+			$pdo->exec(file_get_contents($installer_root . '/../../framework/php/settings/sql/cashmusic_db_sqlite.sql'));
+		} catch (PDOException $e) {
+			echo "\nOh. Shit. Something's wrong: Couldn't write to the database. $e\n\n";
+			die();
+			break;
+		}
+
 		$user_email    = readStdin("\nMain system login email address: ");
-		$system_salt = md5($user_email . time());
+		$system_salt   = md5($user_email . time());
 		$user_password = substr(md5($system_salt . 'password'),4,7);
 		$password_hash = hash_hmac('sha256', $user_password, $system_salt);
-		
+	
 		$data = array(
-		    'email_address' => $user_email,
-		    'password'      => $password_hash,
-		    'creation_date' => time()
+			'email_address' => $user_email,
+			'password'      => $password_hash,
+			'is_admin'      => true,
+			'creation_date' => time()
 		);
-		$query = "INSERT INTO user_users (email_address,password,creation_date) VALUES (:email_address,:password,:creation_date)";
+		$query = "INSERT INTO user_users (email_address,password,is_admin,creation_date) VALUES (:email_address,:password,:is_admin,:creation_date)";
 		
 		try {
-			$q       = $pdo->prepare($query);
+			$q = $pdo->prepare($query);
+		} catch (PDOException $e) {
+			echo "\nOh. Shit. Something's wrong: Couldn't prepare query. $e\n\n";
+			die();
+			break;
+		}
+		
+		try {
 			$success = $q->execute($data);
-			if (!$success) {
-				echo "\nOh. Shit. Something's wrong. Couldn't add the user to the database.\n\n";
-				die();
-				break;
-			}
 		} catch(PDOException $e) {
 			echo "\nOh. Shit. Something's wrong. Couldn't add the user to the database. $e\n\n";
 			die();
 			break;
 		}
-    } 
+	}
 
 	if ($success) {
 		$installer_root = dirname(__FILE__);
@@ -147,17 +185,18 @@ if(!defined('STDIN')) { // force CLI, the browser is *so* 2007...
 		$file_write_success = false;
 		if ($db_engine == "sqlite") {
 			if (
-				findReplaceInFile($installer_root.'/../../framework/php/settings/cashmusic.ini.php','database = "seed','database = "cashmusic.db') ||
+				findReplaceInFile($installer_root.'/../../framework/php/settings/cashmusic.ini.php','driver = "mysql','driver = "sqlite') &&
+				findReplaceInFile($installer_root.'/../../framework/php/settings/cashmusic.ini.php','database = "seed','database = "cashmusic.db') &&
 				findReplaceInFile($installer_root.'/../../framework/php/settings/cashmusic.ini.php','salt = "I was born of sun beams; Warming up our limbs','salt = "' . $system_salt)
 			) {
 				$file_write_success = true;
 			} 
 		} else {
 			if (
-				findReplaceInFile($installer_root.'/../../framework/php/settings/cashmusic.ini.php','hostname = "localhost:8889','hostname = "' . $db_server) || 
-				findReplaceInFile($installer_root.'/../../framework/php/settings/cashmusic.ini.php','username = "root','username = "' . $db_username) || 
-				findReplaceInFile($installer_root.'/../../framework/php/settings/cashmusic.ini.php','password = "root','password = "' . $db_password) || 
-				findReplaceInFile($installer_root.'/../../framework/php/settings/cashmusic.ini.php','database = "seed','database = "' . $db_name) ||
+				findReplaceInFile($installer_root.'/../../framework/php/settings/cashmusic.ini.php','hostname = "localhost:8889','hostname = "' . $db_server) &&
+				findReplaceInFile($installer_root.'/../../framework/php/settings/cashmusic.ini.php','username = "root','username = "' . $db_username) &&
+				findReplaceInFile($installer_root.'/../../framework/php/settings/cashmusic.ini.php','password = "root','password = "' . $db_password) &&
+				findReplaceInFile($installer_root.'/../../framework/php/settings/cashmusic.ini.php','database = "seed','database = "' . $db_name) &&
 				findReplaceInFile($installer_root.'/../../framework/php/settings/cashmusic.ini.php','salt = "I was born of sun beams; Warming up our limbs','salt = "' . $system_salt)
 			) {
 				$file_write_success = true;
@@ -167,7 +206,7 @@ if(!defined('STDIN')) { // force CLI, the browser is *so* 2007...
 			echo "\nOh. Shit. Something's wrong. We had trouble editing a few files. Please try again.\n\n";
 			break;
 		} else {
-			echo "\nSUCCESS!\nYou'll still need to edit /framework/php/settings/cashmusic.ini.php\n\nOnce done, login using:\n\nemail: $user_email\npassword: $user_password\n\n";
+			echo "\nSUCCESS!\n\nLogin using:\n\nemail: $user_email\npassword: $user_password\n\n";
 		}
 	}
 }
