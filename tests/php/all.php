52881f8c0239fa6d8b3bf7f2159b95d4d6f2601d
Jonathan "Duke" Leto <jonathan@leto.net>
2011-09-02 07:55:57 -0700

    attempt to report errors in a better way in the test suite. not quite working yet

--- tests/php/all.php
+++ tests/php/all.php
@@ -1,19 +1,17 @@
 <?php
-require_once('tests/php/base.php');
+//require_once('tests/php/base.php');
+require_once('tests/lib/simpletest/unit_tester.php');
 
-class AllTests extends TestSuite {
-    function AllTests() {
-        $this->TestSuite('All tests');
-        // All tests should be of the form NNN_description.php
-        // Notably, this excludes all.php and base.php, which are special
-        $test_files = glob("tests/php/*_*.php");
-        foreach ($test_files as $test) {
-            $this->addFile($test);
-        }
-        // Since test failures stop the test suite, if we get here, all
-        // tests passed. This lets Jitterbug know what is up.
-        print "Result: PASS\n";
-    }
+$test = new TestSuite('All tests');
+// All tests should be of the form NNN_description.php
+// Notably, this excludes all.php and base.php, which are special
+$test_files = glob("tests/php/*_*.php");
+foreach ($test_files as $file) {
+    $test->addFile($file);
 }
+if (TextReporter::inCli()) {
+    exit ($test->run(new TextReporter()) ? 0 : 1);
+}
+
 ?>
 
