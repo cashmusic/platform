6620453a6d8a783eeda5eb70aa75cf1a6acfa5b8
Jonathan "Duke" Leto <jonathan@leto.net>
2011-09-02 07:55:57 -0700

    attempt to report errors in a better way in the test suite. not quite working yet

--- tests/php/base.php
+++ tests/php/base.php
@@ -1,5 +1,16 @@
 <?php
 
+ini_set('track_errors', 1); 
+error_reporting(-1); // report all errors
+
+function terminator($errno, $errstr, $errfile, $errline)
+{
+	fwrite(STDERR,"$errstr in $errfile line $errline");
+	exit(1);
+}
+
+set_error_handler("terminator");
+
 // All tests should include this file, so we can modify basic test functionality
 // here without needing to modify all our tests
 
