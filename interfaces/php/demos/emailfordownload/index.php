164c992d35453a90f533b7fe675750cfbc3402bf
Jesse von Doom <jesse@cashmusic.org>
2011-09-08 12:53:13 -0700

    email for download now using portugal. the man. assets with no set type check for url and redirect if it is not malformed. - Fixes #22

--- interfaces/php/demos/emailfordownload/index.php
+++ interfaces/php/demos/emailfordownload/index.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>CASH Music : Email For Download</title>
+<title>Email For Download / CASH Music</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link href="assets/css/demo.css" rel="stylesheet" type="text/css" />
 
@@ -11,10 +11,12 @@
 <body>
 
 <div id="mainspc">
-	<h1>BAD BOOKS</h1>
-	<h2>our mailing list</h2>
+	<img src="assets/images/portugaltheman.png" width="325" height="63" alt="Portugal. The Man: Email List" />
+	<p>
+		Join our email list to get a free download of "When The War Ends" from our record <a href="http://store.portugaltheman.com/american-ghetto/"><i>American Ghetto</i></a>.
+	</p>
 
-	<?php cash_embedElement(10); // CASH element (Bad Books Email promo / emailfordownload) ?>
+	<?php cash_embedElement(10); // CASH element (Portugal email for download / emailfordownload) ?>
 	
 	<?php include('/Users/jessevondoom/Code/cashmusic-github/DIY/framework/php/settings/debug/seed_debug.php'); // Debug ?>
 
