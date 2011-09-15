7705200cd266482232b1641ce4d703470cf06554
Jonathan "Duke" Leto <jonathan@leto.net>
2011-09-14 19:15:18 -0700

    Convert run-tests.php to hard tabs as an offering to the Tab Elder Gods

--- tests/php/run-tests.php
+++ tests/php/run-tests.php
@@ -3,8 +3,8 @@ global $argv, $argc;
 
 $test_pattern = $argc > 1 ? $argv[1] : '';
 if (!$test_pattern) {
-    fwrite(STDERR, "Usage: $argv[0] foobar # runs test matching 'foobar'\n");
-    exit(1);
+	fwrite(STDERR, "Usage: $argv[0] foobar # runs test matching 'foobar'\n");
+	exit(1);
 }
 
 require_once('tests/php/base.php');
@@ -14,17 +14,17 @@ $file_pattern = "tests/php/*$test_pattern*.php";
 $test_files   = glob($file_pattern);
 
 foreach ($test_files as $file) {
-    $test->addFile($file);
+	$test->addFile($file);
 }
 
 if (TextReporter::inCli()) {
-    $code = $test->run(new TextReporter()) ? 0 : 1;
-    if ($code == 0) {
-        echo("\nResult: PASS\n");
-    } else {
-        echo("\nResult: FAIL\n");
-    }
-    exit($code);
+	$code = $test->run(new TextReporter()) ? 0 : 1;
+	if ($code == 0) {
+		echo("\nResult: PASS\n");
+	} else {
+		echo("\nResult: FAIL\n");
+	}
+	exit($code);
 }
 
 ?>
