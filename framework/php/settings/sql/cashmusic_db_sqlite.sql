e25c9f13e25da74472e928174695c89ce1094811
Jesse von Doom <jesse@cashmusic.org>
2011-09-15 10:33:37 -0700

    pulled all NOT NULL declarations from sqlite because they seem to be causing more hard than benefit in a sqlite environment. hopefully this clears up some of the insert issues.

--- framework/php/settings/sql/cashmusic_db_sqlite.sql
+++ framework/php/settings/sql/cashmusic_db_sqlite.sql
@@ -9,7 +9,7 @@ BEGIN TRANSACTION;
 -- Table: asst_assets
 --
 CREATE TABLE asst_assets (
-  id INTEGER PRIMARY KEY NOT NULL,
+  id INTEGER PRIMARY KEY,
   user_id int(11) DEFAULT NULL,
   parent_id int(11) DEFAULT NULL,
   location text,
@@ -29,26 +29,26 @@ CREATE INDEX asst_assets_user_id ON asst_assets (user_id);
 -- Table: asst_licenses
 --
 CREATE TABLE asst_licenses (
-  id INTEGER PRIMARY KEY NOT NULL,
-  name text NOT NULL,
-  description text NOT NULL,
-  fulltext blob NOT NULL,
-  uri text NOT NULL
+  id INTEGER PRIMARY KEY,
+  name text,
+  description text,
+  fulltext blob,
+  uri text
 );
 
 --
 -- Table: cmrc_products
 --
 CREATE TABLE cmrc_products (
-  id INTEGER PRIMARY KEY NOT NULL,
+  id INTEGER PRIMARY KEY,
   sku varchar(20) DEFAULT NULL,
   title varchar(100) DEFAULT NULL,
   price decimal(9,2) DEFAULT NULL,
   type varchar(100) DEFAULT NULL,
   beneficiary varchar(50) DEFAULT NULL,
   sub_term_seconds int(11) DEFAULT NULL,
-  qty_total int(11) NOT NULL DEFAULT 0,
-  creation_date int(11) NOT NULL DEFAULT 0,
+  qty_total int(11) DEFAULT 0,
+  creation_date int(11) DEFAULT 0,
   modification_date int(11) DEFAULT NULL
 );
 
@@ -56,26 +56,26 @@ CREATE TABLE cmrc_products (
 -- Table: cmrc_transactions
 --
 CREATE TABLE cmrc_transactions (
-  id INTEGER PRIMARY KEY NOT NULL,
-  order_timestamp varchar(24) NOT NULL DEFAULT '',
-  payer_email varchar(75) NOT NULL DEFAULT '',
-  payer_id varchar(60) NOT NULL DEFAULT '',
-  payer_firstname varchar(127) NOT NULL DEFAULT '',
-  payer_lastname varchar(127) NOT NULL DEFAULT '',
-  country varchar(8) NOT NULL DEFAULT '',
-  product_sku varchar(48) NOT NULL DEFAULT '',
-  product_name varchar(255) NOT NULL DEFAULT '',
-  transaction_id varchar(24) NOT NULL DEFAULT '',
-  transaction_status varchar(32) NOT NULL DEFAULT '',
-  transaction_currency varchar(8) NOT NULL DEFAULT '',
-  transaction_amount int(11) NOT NULL DEFAULT 0,
-  transaction_fee decimal(9,2) NOT NULL DEFAULT 0.00,
-  is_fulfilled smallint(1) NOT NULL DEFAULT 0,
+  id INTEGER PRIMARY KEY,
+  order_timestamp varchar(24) DEFAULT '',
+  payer_email varchar(75) DEFAULT '',
+  payer_id varchar(60) DEFAULT '',
+  payer_firstname varchar(127) DEFAULT '',
+  payer_lastname varchar(127) DEFAULT '',
+  country varchar(8) DEFAULT '',
+  product_sku varchar(48) DEFAULT '',
+  product_name varchar(255) DEFAULT '',
+  transaction_id varchar(24) DEFAULT '',
+  transaction_status varchar(32) DEFAULT '',
+  transaction_currency varchar(8) DEFAULT '',
+  transaction_amount int(11) DEFAULT 0,
+  transaction_fee decimal(9,2) DEFAULT 0.00,
+  is_fulfilled smallint(1) DEFAULT 0,
   referral_code varchar(191) DEFAULT NULL,
   nvp_request_json text,
   nvp_response_json text,
   nvp_details_json text,
-  creation_date int(11) NOT NULL DEFAULT 0,
+  creation_date int(11) DEFAULT 0,
   modification_date int(11) DEFAULT 0
 );
 
@@ -83,7 +83,7 @@ CREATE TABLE cmrc_transactions (
 -- Table: live_events
 --
 CREATE TABLE live_events (
-  id INTEGER PRIMARY KEY NOT NULL,
+  id INTEGER PRIMARY KEY,
   date int(11) DEFAULT NULL,
   user_id int(11) DEFAULT NULL,
   venue_id int(11) DEFAULT NULL,
@@ -100,8 +100,8 @@ CREATE INDEX live_events_user_id ON live_events (user_id);
 -- Table: live_venues
 --
 CREATE TABLE live_venues (
-  id INTEGER PRIMARY KEY NOT NULL,
-  name text NOT NULL,
+  id INTEGER PRIMARY KEY,
+  name text,
   address1 text,
   address2 text,
   city text,
@@ -120,7 +120,7 @@ CREATE TABLE live_venues (
 -- Table: lock_codes
 --
 CREATE TABLE lock_codes (
-  id INTEGER PRIMARY KEY NOT NULL,
+  id INTEGER PRIMARY KEY,
   uid tinytext,
   element_id int(11) DEFAULT NULL,
   claim_date int(11) DEFAULT NULL,
@@ -134,7 +134,7 @@ CREATE INDEX lock_codes_element_id ON lock_codes (element_id);
 -- Table: lock_passwords
 --
 CREATE TABLE lock_passwords (
-  id INTEGER PRIMARY KEY NOT NULL,
+  id INTEGER PRIMARY KEY,
   password text,
   element_id int(11) DEFAULT NULL,
   creation_date int(11) DEFAULT 0,
@@ -147,13 +147,13 @@ CREATE INDEX lock_passwords_element_id ON lock_passwords (element_id);
 -- Table: asst_analytics
 --
 CREATE TABLE asst_analytics (
-  id INTEGER PRIMARY KEY NOT NULL,
-  asset_id int(11) NOT NULL DEFAULT 0,
+  id INTEGER PRIMARY KEY,
+  asset_id int(11) DEFAULT 0,
   element_id int(11) DEFAULT NULL,
-  access_time int(11) NOT NULL,
-  client_ip varchar(39) NOT NULL,
-  client_proxy varchar(39) NOT NULL,
-  cash_session_id varchar(24) NOT NULL,
+  access_time int(11),
+  client_ip varchar(39),
+  client_proxy varchar(39),
+  cash_session_id varchar(24),
   creation_date int(11) DEFAULT NULL,
   modification_date int(11) DEFAULT 0
 );
@@ -164,15 +164,15 @@ CREATE INDEX asst_analytics_asset_id ON asst_analytics (id);
 -- Table: lock_permissions
 --
 CREATE TABLE lock_permissions (
-  id INTEGER PRIMARY KEY NOT NULL,
-  user_id int(11) NOT NULL,
-  user_list_id int(11) NOT NULL DEFAULT 0,
-  element_id int(11) NOT NULL DEFAULT 0,
-  allowed_logins int(11) NOT NULL DEFAULT -1,
-  total_logins int(11) NOT NULL DEFAULT 0,
-  date_expires int(11) NOT NULL DEFAULT -1,
+  id INTEGER PRIMARY KEY,
+  user_id int(11),
+  user_list_id int(11) DEFAULT 0,
+  element_id int(11) DEFAULT 0,
+  allowed_logins int(11) DEFAULT -1,
+  total_logins int(11) DEFAULT 0,
+  date_expires int(11) DEFAULT -1,
   element_password tinytext,
-  added_by int(11) NOT NULL DEFAULT 0,
+  added_by int(11) DEFAULT 0,
   creation_date int(11) DEFAULT NULL,
   modification_date int(11) DEFAULT NULL
 );
@@ -185,10 +185,10 @@ CREATE INDEX lock_permissions_element_id ON lock_permissions (element_id);
 -- Table: user_users
 --
 CREATE TABLE user_users (
-  id INTEGER PRIMARY KEY NOT NULL,
-  email_address varchar(255) NOT NULL DEFAULT '',
-  password char(64) NOT NULL DEFAULT '',
-  username varchar(32) NOT NULL DEFAULT '',
+  id INTEGER PRIMARY KEY,
+  email_address varchar(255) DEFAULT '',
+  password char(64) DEFAULT '',
+  username varchar(32) DEFAULT '',
   display_name tinytext,
   first_name tinytext,
   last_name tinytext,
@@ -199,7 +199,7 @@ CREATE TABLE user_users (
   address_region tinytext,
   address_postalcode tinytext,
   address_country tinytext,
-  is_admin bool NOT NULL DEFAULT '0',
+  is_admin bool DEFAULT '0',
   creation_date int(11) DEFAULT NULL,
   modification_date int(11) DEFAULT NULL
 );
@@ -210,11 +210,11 @@ CREATE INDEX email_address ON user_users (email_address);
 -- Table: user_lists
 --
 CREATE TABLE user_lists (
-  id INTEGER PRIMARY KEY NOT NULL,
-  name varchar(128) NOT NULL DEFAULT '',
+  id INTEGER PRIMARY KEY,
+  name varchar(128) DEFAULT '',
   description text,
-  user_id int(11) NOT NULL,
-  settings_id int(11) NOT NULL,
+  user_id int(11),
+  settings_id int(11),
   creation_date int(11) DEFAULT NULL,
   modification_date int(11) DEFAULT 0
 );
@@ -223,10 +223,10 @@ CREATE TABLE user_lists (
 -- Table: user_resetpassword
 --
 CREATE TABLE user_resetpassword (
-  id INTEGER PRIMARY KEY NOT NULL,
-  time_requested int(11) NOT NULL DEFAULT 0,
-  random_key tinytext NOT NULL,
-  user_id int(11) NOT NULL DEFAULT 0,
+  id INTEGER PRIMARY KEY,
+  time_requested int(11) DEFAULT 0,
+  random_key tinytext,
+  user_id int(11) DEFAULT 0,
   creation_date int(11) DEFAULT NULL,
   modification_date int(11) DEFAULT NULL
 );
@@ -235,10 +235,10 @@ CREATE TABLE user_resetpassword (
 -- Table: elmt_elements
 --
 CREATE TABLE elmt_elements (
-  id INTEGER PRIMARY KEY NOT NULL,
+  id INTEGER PRIMARY KEY,
   user_id int(11) DEFAULT NULL,
   name text,
-  type text NOT NULL,
+  type text,
   options text,
   license_id int(11) DEFAULT 0,
   creation_date int(11) DEFAULT NULL,
@@ -249,16 +249,16 @@ CREATE TABLE elmt_elements (
 -- Table: elmt_analytics
 --
 CREATE TABLE elmt_analytics (
-  id INTEGER PRIMARY KEY NOT NULL,
-  element_id int(11) NOT NULL,
-  access_method varchar(24) NOT NULL,
-  access_location text NOT NULL,
-  access_action text NOT NULL,
-  access_data text NOT NULL,
-  access_time int(11) NOT NULL,
-  client_ip varchar(39) NOT NULL,
-  client_proxy varchar(39) NOT NULL,
-  cash_session_id varchar(24) NOT NULL,
+  id INTEGER PRIMARY KEY,
+  element_id int(11),
+  access_method varchar(24),
+  access_location text,
+  access_action text,
+  access_data text,
+  access_time int(11),
+  client_ip varchar(39),
+  client_proxy varchar(39),
+  cash_session_id varchar(24),
   creation_date int(11) DEFAULT NULL,
   modification_date int(11) DEFAULT 0
 );
@@ -269,11 +269,11 @@ CREATE INDEX elmt_analytics_element_id ON elmt_analytics (element_id);
 -- Table: base_settings
 --
 CREATE TABLE base_settings (
-  id INTEGER PRIMARY KEY NOT NULL,
+  id INTEGER PRIMARY KEY,
   name text,
-  type text NOT NULL,
-  data text NOT NULL,
-  user_id int(11) NOT NULL,
+  type text,
+  data text,
+  user_id int(11),
   creation_date int(11) DEFAULT NULL,
   modification_date int(11) DEFAULT NULL
 );
@@ -282,9 +282,9 @@ CREATE TABLE base_settings (
 -- Table: user_lists_members
 --
 CREATE TABLE user_lists_members (
-  id INTEGER PRIMARY KEY NOT NULL,
-  user_id int(11) NOT NULL,
-  list_id int(11) NOT NULL,
+  id INTEGER PRIMARY KEY,
+  user_id int(11),
+  list_id int(11),
   verification_code text,
   verified bit(1),
   initial_comment text,
@@ -301,11 +301,11 @@ CREATE INDEX user_lists_members_list_id ON user_lists_members (list_id);
 -- Table: live_guestlist
 --
 CREATE TABLE live_guestlist (
-  id INTEGER PRIMARY KEY NOT NULL,
-  event_id int(128) NOT NULL,
+  id INTEGER PRIMARY KEY,
+  event_id int(128),
   guest_name text,
-  total_attendees int(11) NOT NULL DEFAULT 1,
-  comment text NOT NULL,
+  total_attendees int(11) DEFAULT 1,
+  comment text,
   creation_date int(11) DEFAULT NULL,
   modification_date int(11) DEFAULT 0
 );
@@ -314,12 +314,12 @@ CREATE TABLE live_guestlist (
 -- Table: base_metadata
 --
 CREATE TABLE base_metadata (
-  id INTEGER PRIMARY KEY NOT NULL,
-  scope_table_alias varchar(64) NOT NULL DEFAULT '',
-  scope_table_id int(11) NOT NULL DEFAULT 0,
-  user_id int(11) NOT NULL DEFAULT 0,
+  id INTEGER PRIMARY KEY,
+  scope_table_alias varchar(64) DEFAULT '',
+  scope_table_id int(11) DEFAULT 0,
+  user_id int(11) DEFAULT 0,
   type text,
-  value text NOT NULL,
+  value text,
   creation_date int(11) DEFAULT NULL,
   modification_date int(11) DEFAULT NULL
 );
