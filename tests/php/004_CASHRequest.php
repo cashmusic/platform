a70d2c4e259b0dbf522bfa35e982ea378da0c2f1
Jonathan "Duke" Leto <jonathan@leto.net>
2011-09-14 19:57:57 -0700

    Add a test to ensure we can create a new CASHRequest object with an empty array of params

--- tests/php/004_CASHRequest.php
+++ tests/php/004_CASHRequest.php
@@ -5,6 +5,9 @@ require_once('tests/php/base.php');
 class CASHRequestTests extends UnitTestCase {
 
 	function testCASHRequest(){
+		$cr = new CASHRequest(array());
+		$this->assertIsa($cr, 'CASHRequest', 'can create a cash request with no params');
+
 		// TODO: We should be operating on asset id's that actually exist in our test db
 		$cr = new CASHRequest(array(
 			'cash_request_type' => 'asset',
