a5f9f054eb8ab26d3cfcf344d8461057736deb39
Jonathan "Duke" Leto <jonathan@leto.net>
2011-09-20 21:20:44 -0700

    Refactor some duplicate code into a checkSyntax function which isn't as noisy

--- tests/php/000_syntax.php
+++ tests/php/000_syntax.php
@@ -2,26 +2,29 @@
 
 require_once('tests/php/base.php');
 
+
 class SyntaxTests extends UnitTestCase {
+    public function checkSyntax($file) {
+        $output = exec("php -l $file", $array, $code);
+        $this->assertTrue($code == 0, $output);
+    }
+
     public function testFrameworkSyntax() {
         $test_files = glob("{framework/php/cashmusic.php,framework/php/*/*.php,framework/php/classes/*/*.php}", GLOB_BRACE);
         foreach ($test_files as $file){
-            system("php -l $file", $code);
-            $this->assertTrue($code == 0);
+            $this->checkSyntax($file);
         }
     }
     public function testInstallerSyntax() {
         $test_files = glob("installers/php/*.php");
         foreach ($test_files as $file){
-            system("php -l $file", $code);
-            $this->assertTrue($code == 0);
+            $this->checkSyntax($file);
         }
     }
     public function testInterfaceSyntax() {
         $test_files = glob("{interfaces/php/*/*.php,interfaces/php/*/*/*.php,interfaces/php/*/*/*/*.php,interfaces/php/*/*/*/*/*.php}", GLOB_BRACE);
         foreach ($test_files as $file){
-            system("php -l $file", $code);
-            $this->assertTrue($code == 0);
+            $this->checkSyntax($file);
         }
     }
 }
