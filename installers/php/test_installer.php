39974348271e1935e62d33fe246acdb155b3c5f4
Jonathan "Duke" Leto <jonathan@leto.net>
2011-09-25 13:26:51 -0700

    Make the test install use a different sqlite db and fix sqlite permissions for test installs

--- installers/php/test_installer.php
+++ installers/php/test_installer.php
@@ -41,8 +41,8 @@ if(!defined('STDIN')) { // force CLI, the browser is *so* 2007...
 	$installer_root = dirname(__FILE__);
 
 	// if the file exists already, rename it as a backup
-	if (file_exists($installer_root . '/../../framework/php/db/cashmusic.db')) {
-		rename($installer_root . '/../../framework/php/db/cashmusic.db',$installer_root . '/../../framework/php/db/cashmusic.db.bak');
+	if (file_exists($installer_root . '/../../framework/php/db/cashmusic_test.db')) {
+		rename($installer_root . '/../../framework/php/db/cashmusic_test.db',$installer_root . '/../../framework/php/db/cashmusic_test.db.bak');
 	} else {
 		// if the directory was never created then create it now
 		if (!file_exists($installer_root . '/../../framework/php/db')) {
@@ -52,12 +52,17 @@ if(!defined('STDIN')) { // force CLI, the browser is *so* 2007...
 	
 	// connect to the new db...will create if not found
 	try {
-		$pdo = new PDO ('sqlite:' . $installer_root . '/../../framework/php/db/cashmusic.db');
+		$pdo = new PDO ('sqlite:' . $installer_root . '/../../framework/php/db/cashmusic_test.db');
 		$pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 	} catch (PDOException $e) {
 		echo "\nOh. Shit. Something's wrong: Couldn't connect to the database. $e\n\n";
 		die();
 		break;
+	}
+	// TODO: Suboptimal
+	if ($pdo) {
+		chmod($installer_root . '/../../framework/php/db',0777);
+		chmod($installer_root . '/../../framework/php/db/cashmusic_test.db',0777);
 	}
 	
 	// push in all the tables
@@ -114,7 +119,7 @@ if(!defined('STDIN')) { // force CLI, the browser is *so* 2007...
 		if (
 			findReplaceInFile($installer_root.'/../../framework/php/settings/cashmusic.ini.php','driver = "mysql','driver = "sqlite') &&
 			findReplaceInFile($installer_root.'/../../framework/php/settings/cashmusic.ini.php','debug = 0','debug = 1') &&
-			findReplaceInFile($installer_root.'/../../framework/php/settings/cashmusic.ini.php','database = "seed','database = "cashmusic.db') &&
+			findReplaceInFile($installer_root.'/../../framework/php/settings/cashmusic.ini.php','database = "seed','database = "cashmusic_test.db') &&
 			findReplaceInFile($installer_root.'/../../framework/php/settings/cashmusic.ini.php','salt = "I was born of sun beams; Warming up our limbs','salt = "' . $system_salt)
 		) {
 			$file_write_success = true;
