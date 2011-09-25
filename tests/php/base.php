cdc5f1fb62169d446beb4a45368e57dde9afe54c
Jonathan "Duke" Leto <jonathan@leto.net>
2011-09-25 12:05:19 -0700

    Run the test installer before including any code into tests/php/base.php

--- tests/php/base.php
+++ tests/php/base.php
@@ -14,6 +14,9 @@ function terminator($errno, $errstr, $errfile, $errline)
 
 set_error_handler("terminator");
 
+// this deploys to SQLite and sets up cashmusic.ini.php
+system("php installers/php/test_installer.php");
+
 // All tests should include this file, so we can modify basic test functionality
 // here without needing to modify all our tests
 
@@ -25,8 +28,5 @@ require_once('tests/lib/simpletest/reporter.php');
 require_once('framework/php/cashmusic.php');
 
 require_once('framework/php/classes/seeds/S3Seed.php');
-
-// this deploys to SQLite and sets up cashmusic.ini.php
-system("php installers/php/test_installer.php");
 
 ?>
