bc3a98d71ce764f79553f25c9eb8c4b180bcdcb0
Jesse von Doom <jesse@cashmusic.org>
2011-10-05 21:09:30 -0700

    working on social feed admin stuff

---
+++ framework/php/classes/elements/SocialFeeds.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Email For Download element
+ *
+ * @package diy.org.cashmusic
+ * @author CASH Music
+ * @link http://cashmusic.org/
+ *
+ * Copyright (c) 2011, CASH Music
+ * Licensed under the Affero General Public License version 3.
+ * See http://www.gnu.org/licenses/agpl-3.0.html
+ *
+ **/
+class SocialFeeds extends ElementBase {
+	const type = 'socialfeeds';
+	const name = 'Social Feeds';
+
+	public function getMarkup() {
+		$markup = '';
+		$tourdates_request = new CASHRequest(
+			array(
+				'cash_request_type' => 'calendar', 
+				'cash_action' => 'gettourdates',
+				'visible_event_types' => $this->options->visible_event_types,
+				'user_id' => (integer) $this->element['user_id']
+			)
+		);
+		if ($tourdates_request->response['status_uid'] == "calendar_gettourdates_200") {
+			// spit out the dates
+			foreach ($tourdates_request->response['payload'] as $event) {
+				$event_location = $event['venue_city'] . ', ' . $event['venue_country'];
+				if (strtolower($event['venue_country']) == 'usa' || strtolower($event['venue_country']) == 'canada') {
+					$event_location = $event['venue_city'] . ', ' . $event['venue_region'];
+				}
+				$markup .= '<div class="cash_'. self::type .'_event"> '
+						. '<div class="cash_'. self::type .'_timeandplace"> '
+						. '<span class="cash_'. self::type .'_date">' . date('d F, Y',$event['date']) . ':</span> '
+						. '<span class="cash_'. self::type .'_location">' . $event_location . '</span> '
+						. '<span class="cash_'. self::type .'_venue">@ ' . $event['venue_name'] . '</span> '
+						. '</div> ';
+				if ($event['comments']) {
+					$markup .= '<span class="cash_'. self::type .'_comments">' . $event['comments'] . '</span> ';
+				}
+				if ($event['purchase_url']) {
+					$markup .= '<span class="cash_'. self::type .'_purchase_url"><a href="' . $event['purchase_url'] . '" class="external">Tickets</a></span> ';
+				}
+				if ($event['venue_address1'] && $event['venue_city'] && $event['venue_country']) {
+					$markup .= '<span class="cash_'. self::type .'_purchase_url"><a href="http://maps.google.com/maps?f=q&hl=en&geocode=&q=' . $event['venue_address1'] . '+' . $event['venue_city'] . '+' . $event['venue_region'] . '+' . $event['venue_country'] . '+(' . $event['venue_name'] . ')" class="external">Map</a></span> ';
+				}
+				$markup .= '</div>';
+			}
+		} else {
+			// no dates matched
+			$markup .= 'There are no dates to display right now.';
+		}
+		return $markup;	
+	}
+} // END class 
+?>
