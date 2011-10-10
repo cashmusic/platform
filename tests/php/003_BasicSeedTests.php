cb6a668e59ffac28896745862e84854dc143b1d7
Jonathan "Duke" Leto <jonathan@leto.net>
2011-10-09 21:11:21 -0700

    Add some unit tests for the mailchimp seed

--- tests/php/003_BasicSeedTests.php
+++ tests/php/003_BasicSeedTests.php
@@ -3,17 +3,20 @@
 require_once('tests/php/base.php');
 
 class CashSeedTests extends UnitTestCase {
-
-    function testS3Seed(){
-        $settings = new S3Seed(1,1);
-        $this->assertIsa($settings, 'S3Seed');
-    }
-    function testTwitterSeed(){
-        $user_id      = 1;
-        $settings_id  = 1;
-        $twitter      = new TwitterSeed($user_id,$settings_id);
-        $this->assertIsa($twitter, 'TwitterSeed');
-    }
-
+	function testS3Seed(){
+		$settings = new S3Seed(1,1);
+		$this->assertIsa($settings, 'S3Seed');
+	}
+	function testTwitterSeed(){
+		$user_id      = 1;
+		$settings_id  = 1;
+		$twitter      = new TwitterSeed($user_id,$settings_id);
+		$this->assertIsa($twitter, 'TwitterSeed');
+	}
+	function testMailchimpSeed(){
+		$settings = new MailchimpSeed('keyblarg-us42');
+		$this->assertIsa($settings, 'MailchimpSeed');
+		$this->assertTrue($settings->url);
+	}
 }
 ?>
