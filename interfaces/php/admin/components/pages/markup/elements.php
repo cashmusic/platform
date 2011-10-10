a60cb5b53fc4a365afe1cf9d00e097c4e10834f7
Jonathan "Duke" Leto <jonathan@leto.net>
2011-10-09 17:44:00 -0700

    Update list of elements in progress

--- interfaces/php/admin/components/pages/markup/elements.php
+++ interfaces/php/admin/components/pages/markup/elements.php
@@ -36,8 +36,6 @@
 	<li>Tweet for download</li>
 	<li>FB like for download</li>
 	<li>Download for current social followers</li>
-	<li>Social Feeds</li>
-	<li>Email for download</li>
 	<li>Email capture</li>
 	<li>Download for current mailing list participants</li>
 	<li>Download codes</li>
