7fd4084d63676b62e06f88ef33ba10cec0840ef6
Jesse von Doom <jesse@cashmusic.org>
2011-10-13 19:55:14 -0700

    hooked up the main calendar page

--- interfaces/php/admin/components/pages/markup/calendar.php
+++ interfaces/php/admin/components/pages/markup/calendar.php
@@ -1,1 +1,8 @@
-In progress.
+<div class="col_onehalf">
+	<h2>This week at a glance</h2>
+	<?php echo calendar_format_dates($cash_admin->getStoredResponse('events_thisweek')); ?>
+</div>
+<div class="col_onehalf lastcol">
+	<h2>Unpublished events</h2>
+	<?php echo calendar_format_dates($cash_admin->getStoredResponse('events_unpublished')); ?>
+</div>
