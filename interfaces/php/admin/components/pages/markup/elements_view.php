3796b5bc35139d6ee783cd57b12724df226e2202
Jesse von Doom <jesse@cashmusic.org>
2011-10-16 16:21:39 -0700

    more minor style changes. adding a few notes, etc.

--- interfaces/php/admin/components/pages/markup/elements_view.php
+++ interfaces/php/admin/components/pages/markup/elements_view.php
@@ -8,7 +8,7 @@ if ($request_parameters) {
 			&lt;?php cash_embedElement(<?php echo $page_request->response['payload']['id']; ?>); // CASH element (<?php echo $page_request->response['payload']['name'] . ' / ' . $page_request->response['payload']['type']; ?>) ?&gt;
 		</code>
 		<br /><br />
-		Add usage statistics, embed locations, any other analytics...
+		<span class="highlightcopy">Add usage statistics, embed locations, any other analytics...</span>
 		<?php
 	} else {
 		echo "There was a problem getting the element's details. Please <a href=\"" . ADMIN_WWW_BASE_PATH . "/elements/view/\">try again</a>.";
