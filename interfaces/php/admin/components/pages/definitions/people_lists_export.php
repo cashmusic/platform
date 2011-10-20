6ae6743d59e7407784b833f1eba74f980062472d
Jesse von Doom <jesse@cashmusic.org>
2011-10-20 15:47:47 -0700

    fun with escape characters

--- interfaces/php/admin/components/pages/definitions/people_lists_export.php
+++ interfaces/php/admin/components/pages/definitions/people_lists_export.php
@@ -13,15 +13,14 @@ $list_details = $cash_admin->requestAndStore(
 	'listdetails'
 );
 if (isset($list_details)) {
-	header('Content-Disposition: attachment; filename="list_' . $request_list_id . '_export.csv"');
+	//header('Content-Disposition: attachment; filename="list_' . $request_list_id . '_export.csv"');
 	if ($list_details['status_uid'] == 'people_viewlist_200') {
-		echo '"email address","display name","initial comment","additional data","active","join date"' . "\n";
+		echo '"email address","display name","initial comment","additional data","join date"' . "\n";
 		foreach ($list_details['payload']['members'] as $entry) {
 		    echo '"' . $entry['email_address'] . '"';
-			echo ',"' . $entry['display_name'] . '"';
-			echo ',"' . $entry['initial_comment'] . '"';
-			echo ',"' . $entry['additional_data'] . '"';
-			echo ',"' . $entry['active'] . '"';
+			echo ',"' . str_replace ('"','""',$entry['display_name']) . '"';
+			echo ',"' . str_replace ('"','""',$entry['initial_comment']) . '"';
+			echo ',"' . str_replace ('"','""',$entry['additional_data']) . '"';
 			echo ',"' . date('M j, Y h:iA T',$entry['creation_date']) . '"';
 			echo "\n";
 		}
