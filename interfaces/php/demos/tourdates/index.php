a71c641359f1816664812bb88683a7e14246a178
Jesse von Doom <jesse@cashmusic.org>
2011-11-02 11:44:56 -0700

    added a meta tag to hopefully fix android media queries

--- interfaces/php/demos/tourdates/index.php
+++ interfaces/php/demos/tourdates/index.php
@@ -3,6 +3,7 @@
 <head> 
 <title>Tour Dates / CASH Music</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width" />
 <link href="assets/css/demo.css" rel="stylesheet" type="text/css" />
 
 <!--[if !IE]>-->
