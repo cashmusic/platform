8857f878ed9f6bd018b35783bb38f71e3874a7a5
Jesse von Doom <jesse@cashmusic.org>
2011-11-17 11:30:41 -0800

    fitmk

--- framework/php/classes/plants/SystemPlant.php
+++ framework/php/classes/plants/SystemPlant.php
@@ -80,6 +80,14 @@ class SystemPlant extends PlantBase {
 						return $this->pushFailure('there was an error');
 					}
 					break;
+				case 'fitmk':
+					// for Duke...you're awesome.
+					$litany = "I must not fear. Fear is the mind-killer."
+							 . "Fear is the little-death that brings total obliteration."
+							 . "I will face my fear. I will permit it to pass over me and through me."
+							 . "And when it has gone past I will turn the inner eye to see its path."
+							 . "Where the fear has gone there will be nothing. Only I will remain. -- Frank Herbert";
+					return $this->pushSuccess($litany,'check it');
 				default:
 					return $this->response->pushResponse(
 						400,$this->request_type,$this->action,
