d5164c486092d86f035a8d24cdcff18bbecffb03
Jesse von Doom <jesse@cashmusic.org>
2011-11-17 14:24:34 -0800

    generating the correct API key in doListSync

--- framework/php/classes/plants/SystemPlant.php
+++ framework/php/classes/plants/SystemPlant.php
@@ -82,10 +82,10 @@ class SystemPlant extends PlantBase {
 					break;
 				case 'fitmk':
 					// for Duke...you're awesome.
-					$litany = "I must not fear. Fear is the mind-killer."
-							 . "Fear is the little-death that brings total obliteration."
-							 . "I will face my fear. I will permit it to pass over me and through me."
-							 . "And when it has gone past I will turn the inner eye to see its path."
+					$litany = "I must not fear. Fear is the mind-killer. "
+							 . "Fear is the little-death that brings total obliteration. "
+							 . "I will face my fear. I will permit it to pass over me and through me. "
+							 . "And when it has gone past I will turn the inner eye to see its path. "
 							 . "Where the fear has gone there will be nothing. Only I will remain. -- Frank Herbert";
 					return $this->pushSuccess($litany,'check it');
 				default:
