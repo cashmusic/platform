eafac8a8e19c45645b99eadb90b4fbb9bd102d9f
Jesse von Doom <jesse@cashmusic.org>
2011-12-08 11:22:35 -0800

    e-card summary
    moving too fast before, so i stupided my way through 2 weirdo commits. this one
    adds the e-card and all relevant files.

---
+++ interfaces/php/admin/components/elements/ecard/edit.php
@@ -0,0 +1,138 @@
+<?php 
+$page_data = $page_request->response['payload'];
+if (isset($_POST['doelementedit'])) {
+	if (isset($_POST['do_not_verify'])) {
+		$do_not_verify = 1;
+	} else {
+		$do_not_verify = 0;
+	}
+	$element_edit_request = new CASHRequest(
+		array(
+			'cash_request_type' => 'element', 
+			'cash_action' => 'editelement',
+			'id' => $page_request->response['payload']['id'],
+			'name' => $_POST['element_name'],
+			'options_data' => array(
+				'message_invalid_email' => $_POST['message_invalid_email'],
+				'message_instructions' => $_POST['message_instructions'],
+				'image_url' => $_POST['image_url'],
+				'email_subject' => $_POST['email_subject'],
+				'email_message' => $_POST['email_message'],
+				'email_html_message' => $_POST['email_html_message'],
+				'message_success' => $_POST['message_success'],
+				'emal_list_id' => $_POST['emal_list_id'],
+				'asset_id' => $_POST['asset_id'],
+				'do_not_verify' => $do_not_verify
+			)
+		)
+	);
+	if ($element_edit_request->response['status_uid'] == 'element_editelement_200') {
+		$page_data = $element_edit_request->response['payload'];
+	?>
+	
+		<h3>Success</h3>
+		<p>
+		</p>
+		Your edits have been made and can be seen below. To embed the element us this code:
+		<code>
+			&lt;?php CASHSystem::embedElement(<?php echo $page_data['id']; ?>); // CASH element (<?php echo $page_data['name'] . ' / ' . $page_data['type']; ?>) ?&gt;
+		</code>
+		<br />
+
+	<?php } else { ?>
+		
+		<h3>Error</h3>
+		<p>
+		There was a problem editing the element. <a href="./">Please try again.</a>
+		</p>
+
+<?php
+	}
+}
+?>
+	<form method="post" action="">
+		<input type="hidden" name="doelementedit" value="makeitso">
+		<input type="hidden" name="element_id" value="<?php echo $page_data['id']; ?>">
+		<h3>Element Details</h3>
+
+		<label for="element_name">Name</label><br />
+		<input type="text" id="element_name" name="element_name" value="<?php echo $page_data['name']; ?>" /> 
+
+		<div class="row_seperator">.</div>
+		<label for="message_instructions">Instructions Message</label><br />
+		<input type="text" id="message_instructions" name="message_instructions" value="<?php echo $page_data['options']->message_instructions; ?>" />
+
+		<div class="row_seperator">.</div>
+		<label for="image_url">Card Image URL</label><br />
+		<input type="text" id="image_url" name="image_url" value="<?php echo $page_data['options']->image_url; ?>" />
+
+		<div class="row_seperator">.</div>
+		<div class="col_oneoftwo">
+
+			<label for="asset_id">Target Mailing List</label><br />
+			<select id="emal_list_id" name="emal_list_id">
+				<option value="0">none</option>
+				<?php AdminHelper::echoFormOptions('user_lists',$page_data['options']->emal_list_id); ?>
+			</select>
+			<br /><br />
+			<?php
+			if ($page_data['options']->do_not_verify) {
+				$checkstring = "checked='checked'";
+				
+			} else {
+				$checkstring = "";
+			}
+			?>
+			<input type='checkbox' class='checkorradio' name='do_not_verify' value='' <?php echo $checkstring; ?> /> <label for="do_not_verify">Skip email verification</label>
+
+		</div>
+		<div class="col_oneoftwo lastcol">
+
+			<label for="asset_id">The Downloadable Asset</label><br />
+			<select id="asset_id" name="asset_id">
+				<option value="0">none</option>
+				<?php AdminHelper::echoFormOptions('assets',$page_data['options']->asset_id); ?>
+			</select>
+			
+			<br /><br />
+	
+			<a href="<?php echo ADMIN_WWW_BASE_PATH; ?>/assets/add/"><small>OR ADD NEW ASSET</small></a>
+			
+		</div>
+
+		<div class="row_seperator">.</div>
+		<br />
+		<label for="message_invalid_email">Invalid Email Error Message</label><br />
+		<input type="text" id="message_invalid_email" name="message_invalid_email" value="<?php echo $page_data['options']->message_invalid_email; ?>" />
+
+		<div class="row_seperator">.</div>
+		<label for="message_success">Success Message</label><br />
+		<input type="text" id="message_success" name="message_success" value="<?php echo $page_data['options']->message_success; ?>" />
+
+		<div class="row_seperator">.</div><br />
+		<label for="email_subject">Friend Email Subject (Their email address will be appended)</label><br />
+		<input type="text" id="email_subject" name="email_subject" value="<?php echo $page_data['options']->email_subject; ?>" />
+		
+		<div class="row_seperator">.</div>
+		<label for="email_message">Friend Email Message (Plain text)</label><br />
+		<textarea id="email_message" name="email_message"><?php echo $page_data['options']->email_message; ?></textarea>
+		
+		<div class="row_seperator">.</div>
+		<label for="email_html_message">Friend Email Message (HTML - blank will style plain text)</label><br />
+		<textarea id="email_html_message" name="email_html_message"><?php echo $page_data['options']->email_html_message; ?></textarea>
+
+		<!--
+		<div class="row_seperator">.</div><br />
+
+		<label for="comment_or_radio">Comment Or Agreement</label><br />
+		<input type="radio" name="comment_or_radio" class="checkorradio" value="none" checked="checked" /> Neither &nbsp; &nbsp; <input type="radio" name="comment_or_radio" class="checkorradio" value="comment" /> Comment &nbsp; &nbsp; <input type="radio" name="comment_or_radio" class="checkorradio" value="agreement" /> Agreement 
+		
+		-->
+
+		<div class="row_seperator">.</div>
+		<div>
+			<br />
+			<input class="button" type="submit" value="Edit The Element" />
+		</div>
+
+	</form>
