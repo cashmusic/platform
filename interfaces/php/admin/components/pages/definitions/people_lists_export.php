818bb735e7f461f12c33464754bfe1de7e334036
Jesse von Doom <jesse@cashmusic.org>
2011-12-17 01:47:19 -0800

    reenabled content-disposition attachment for list export...

--- interfaces/php/admin/components/pages/definitions/people_lists_export.php
+++ interfaces/php/admin/components/pages/definitions/people_lists_export.php
@@ -13,7 +13,7 @@ $list_details = $cash_admin->requestAndStore(
 	'listdetails'
 );
 if (isset($list_details)) {
-	//header('Content-Disposition: attachment; filename="list_' . $request_list_id . '_export.csv"');
+	header('Content-Disposition: attachment; filename="list_' . $request_list_id . '_export.csv"');
 	if ($list_details['status_uid'] == 'people_viewlist_200') {
 		echo '"email address","display name","initial comment","additional data","join date"' . "\n";
 		foreach ($list_details['payload']['members'] as $entry) {
