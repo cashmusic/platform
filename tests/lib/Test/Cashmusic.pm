7a5d332d8bc0e76f04e468ebe9859ec7ad5d586e
Jonathan "Duke" Leto <jonathan@leto.net>
2012-01-08 08:09:46 -0800

    Document the awesomeness of Test::Cashmusic

--- tests/lib/Test/Cashmusic.pm
+++ tests/lib/Test/Cashmusic.pm
@@ -1,4 +1,33 @@
+# Copyright (C) 2011-2012, Leto Labs, LLC
 package Test::Cashmusic;
+
+=head1 NAME
+
+Test::Cashmusic - Convenience testing functions for CASH Music
+
+=head1 SYNOPSIS
+
+    # import testing functions into current scope
+    use Test::Cashmusic qw/mech mech_success_ok login_ok json_ok/;
+
+    my $m = mech();
+
+    # pass a test if we can login properly
+    login_ok();
+
+    # tests that require logging in (i.e. admin panel)
+    $m->get_ok("http://localhost:80/admin/foo/bar/baz");
+
+=head1 DESCRIPTION
+
+This Perl 5 module contains function which make it easier to write
+Perl integration tests for CASH Music.
+
+=head1 AUTHOR
+
+Jonathan "Duke" Leto
+
+=cut
 
 use strict;
 use warnings;
@@ -19,13 +48,37 @@ BEGIN {
     qx "php installers/php/test_installer.php";
 }
 
+=head2 mech
+
+    my $m = mech();
+
+Returns a Test::WWW::Mechanize object.
+
+=cut
+
 sub mech {
     $mech
 }
 
+=head2 mech_sucess_ok
+
+    mech_success_ok();
+
+Passes a test if the most recent content contains the string 'Success'.
+
+=cut
+
 sub mech_success_ok {
     mech->content_like(qr/Success/) or diag mech->content;
 }
+
+=head2 login_ok
+
+    login_ok();
+
+Passes some tests assuring that we can login properly.
+
+=cut
 
 sub login_ok {
     mech->get_ok("$base/interfaces/php/admin/");
@@ -42,6 +95,22 @@ sub login_ok {
     return mech;
 }
 
+=head2 json_ok
+
+    my ($url,$method) = ("http://localhost:80","get");
+
+    # Pass/fail a test if GETting $url is valid json
+    # It also returns the json for further testing
+    my $json = json_ok($url, $method);
+
+    # Pass/fail a test if POSTing $data to $url is valid json
+    my ($url,$method, $data) = ("http://localhost:80","post", "foobar");
+    my $json = json_ok($url, $method, $data);
+
+Passes a test if the specified $url returns valid JSON.
+
+=cut
+
 sub json_ok {
     my ($url, $method, $data) = @_;
     $method ||= 'get';
@@ -52,6 +121,5 @@ sub json_ok {
     # return the JSON for further testing
     return $json;
 }
-
 
 1;
