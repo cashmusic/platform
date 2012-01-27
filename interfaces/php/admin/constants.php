09a06c9572ea54fdde3ad7ef22e642f54655ca84
Jesse von Doom <jesse@cashmusic.org>
2012-01-26 22:21:57 -0800

    missed these in the last commit. because i am dumm.

---
+++ interfaces/php/admin/constants.php
@@ -0,0 +1,10 @@
+<?php
+// include the necessary bits, define the page directory
+// Define constants too
+$root = dirname(__FILE__);
+$cashmusic_root = $root . "/../../../framework/php/cashmusic.php";
+
+define('ADMIN_BASE_PATH', $root);
+define('ADMIN_WWW_BASE_PATH', '/interfaces/php/admin');
+define('CASH_PLATFORM_PATH', $cashmusic_root);
+?>
