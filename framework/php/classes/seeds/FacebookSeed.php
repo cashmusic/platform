f5f5502f93de9d3df86a4af0d5871308ead1ed95
Jesse von Doom <jesse@cashmusic.org>
2012-02-01 23:51:12 -0800

    basic user/page information gathered from public APIs on Facebook and Twitter as part of their respective seeds - Fixes #114, Fixes #115

---
+++ framework/php/classes/seeds/FacebookSeed.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Facebook library wrapper and public feed fetcher
+ *
+ * @package diy.org.cashmusic
+ * @author CASH Music
+ * @link http://cashmusic.org/
+ *
+ * Copyright (c) 2011, CASH Music
+ * Licensed under the Affero General Public License version 3.
+ * See http://www.gnu.org/licenses/agpl-3.0.html
+ *
+ **/
+class FacebookSeed extends SeedBase {
+	protected $twitter;
+
+	public function __construct($user_id=false,$connection_id=false) {
+		$this->settings_type = 'com.facebook';
+		$this->user_id = $user_id;
+		$this->connection_id = $connection_id;
+		$this->primeCache();
+		if ($user_id && $connection_id) {
+			$this->connectDB();
+			if ($this->getCASHConnection()) {
+				// fire up an instance of the lib
+			} else {
+				// error out — potentially to special error message page.
+			}
+		}
+	}
+
+	public function getUserOrPage($id) {
+		$endoint_url = 'https://graph.facebook.com/' . $id;
+		$user_data = json_decode(CASHSystem::getURLContents($endoint_url),true);
+		return $user_data;
+	}
+	
+} // END class 
+?>
