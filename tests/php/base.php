a4b070e40741d647b0e579c5db19483481470b4a
Jesse von Doom <jesse@cashmusic.org>
2012-04-04 14:38:29 -0700

    trying to shore up tests to solve jitterbug fails

--- tests/php/base.php
+++ tests/php/base.php
@@ -1,5 +1,4 @@
 <?php
-
 // why is it so hard to tell PHP you want to see errors?
 ini_set('track_errors', 1);
 error_reporting(-1); // report all errors
@@ -11,13 +10,26 @@ function terminator($errno, $errstr, $errfile, $errline)
 	fwrite(STDERR,"$errstr in $errfile line $errline");
 	exit(1);
 }
-
 set_error_handler("terminator");
 
-require_once('tests/php/functions.php');
+// Get environment variables from local file or from environment
+function getTestEnv($varname) {
+	if (file_exists(dirname(__FILE__) . '/__test_environment.json')) {
+		$environment = json_decode(file_get_contents(dirname(__FILE__) . '/__test_environment.json'),true);
+		if (isset($environment[$varname])) {
+			return $environment[$varname];
+		} else {
+			return false;
+		}
+	} else {
+		return getenv($varname);
+	}
+}
 
 // this deploys to SQLite and sets up cashmusic.ini.php
+// sleep to avoid race condition if PHP is set to handle system as sub-process
 system("php installers/php/test_installer.php");
+sleep(3);
 
 // All tests should include this file, so we can modify basic test functionality
 // here without needing to modify all our tests
