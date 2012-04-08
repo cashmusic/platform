91ec3b97b0039f4afdd7626c16a5d8405ab19d26
Jesse von Doom <jesse@cashmusic.org>
2012-04-07 21:10:44 -0700

    breadcrumb trail. take that hansel.

--- interfaces/php/admin/components/pages/markup/assets_browse.php
+++ interfaces/php/admin/components/pages/markup/assets_browse.php
@@ -17,6 +17,20 @@ if (is_array($applicable_connections)) {
 			echo '<span class="icon folder_fill"></span> ' . basename($browse_path);
 		}
 		echo '</h3>';
+		
+		if ($browse_path != '.') {
+			$current_link = ADMIN_WWW_BASE_PATH . '/assets/browse/connection/' . $connection_id;
+			echo '<span class="smalltext fadedtext"><a href="' . $current_link . '" class="fadedtext">' . '<span class="icon box"></span> '. $connection_name . '</a>';
+			$exploded_location = explode('/',$browse_path);
+			foreach ($exploded_location as $level) {
+				$current_link .= '/' . $level;
+				echo ' / <a href="' . $current_link . '" class="fadedtext">' . $level . '</a>';
+			}
+			echo '</span><div class="row_seperator">.</div>';
+		} else {
+			echo '<div class="row_seperator">.</div>';
+		}
+		
 		echo '<ul class="assetbrowse">';
 		foreach ($list_assets['directories'] as $location => $directory) {
 			echo '<li><a href="' . ADMIN_WWW_BASE_PATH . '/assets/browse/connection/' . $connection_id . '/' . $location . '"><span class="icon folder_fill"></span> <b>/ ' . basename($directory) . '</b></a></li>';
