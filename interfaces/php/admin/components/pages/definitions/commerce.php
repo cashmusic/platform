d6e793218549c89623736307548504eba69641b2
Jesse von Doom <jesse@cashmusic.org>
2012-04-11 15:50:52 -0700

    changing the connection icon inasset browse to make the commerce item icon better. duh.

--- interfaces/php/admin/components/pages/definitions/commerce.php
+++ interfaces/php/admin/components/pages/definitions/commerce.php
@@ -2,4 +2,12 @@
 // add unique page settings:
 $page_title = 'Commerce: Main';
 $page_tips = '';
+
+$page_memu = array(
+	'Assets' => array(
+		'commerce/items/' => array('Items','box'),
+		'commerce/orders/' => array('Orders','book')
+	)
+);
+
 ?>
