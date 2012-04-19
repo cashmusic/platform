74fdd8f475a584f48d0468c0588c30634d2febe7
Jesse von Doom <jesse@cashmusic.org>
2012-04-18 22:43:33 -0700

    new/better scheme for download/unlock codes

--- interfaces/php/demos/sandbox/index.php
+++ interfaces/php/demos/sandbox/index.php
@@ -12,15 +12,6 @@
 		<?php CASHSystem::embedElement(113); ?> 
 	</div>
 
-<?php
-	$addcode_request = new CASHRequest(
-		array(
-			'cash_request_type' => 'element', 
-			'cash_action' => 'addlockcode',
-			'element_id' => 113
-		)
-	);
-
 <?php include('../../../../framework/php/settings/debug/cashmusic_debug.php'); // Debug ?>
 </body> 
 </html>
