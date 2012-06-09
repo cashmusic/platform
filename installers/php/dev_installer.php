b215b857205bca42b2495522b434afe5d16a407f
Jesse von Doom <jesse@cashmusic.org>
2012-06-09 14:19:04 -0700

    Improving the dev installer
      - Added the option to use default system salt (easier for multiple reinstalls / fixing fuck-ups)
      - Split out user settings to avoid DRY errors
      - Fixed MySQL error where port was being stripped from cashmusic.ini.php
      - Added a prompt for password. All the temporary passwords were just bad form

--- installers/php/dev_installer.php
+++ installers/php/dev_installer.php
@@ -78,24 +78,17 @@ echo "\n                       :+#\n"
 	echo "\n\nYou are in an open field west of a big white house with a boarded front door.\n\n";
 	// you can input <Enter> or 1, 2, 3 
 	$db_engine = readStdin('What database engine do you want to use? (\'mysql\'|\'sqlite\'): ', array('mysql', 'sqlite'));
+
 	if ($db_engine == 'mysql') {
 		if (@new PDO()) {
 			echo "\nOh. Shit. Something's wrong: PDO is required.\n\n";
 			die();
 		}
 		
-		$db_server     = readStdin('Database server (default: \'127.0.0.1:3306\'): ', false,'127.0.0.1:3306');
-		$db_name       = readStdin('Database name: ');
-		$db_username   = readStdin('Database username: ');
-		$db_password   = readStdin('Database password: ');
-		$user_email    = readStdin("\nMain system login email address: ");
-		$default_salt  = readStdin("\nUse defaut system salt? (y/n): ", false, 'y');
-
-		if (strtolower(substr($default_salt,0,1)) == 'y') {
-			$system_salt = 'I was born of sun beams; Warming up our limbs';
-		} else {
-			$system_salt = md5($user_email . time());
-		}
+		$db_server      = readStdin('Database server (default: \'127.0.0.1:3306\'): ', false,'127.0.0.1:3306');
+		$db_name        = readStdin('Database name: ');
+		$db_username    = readStdin('Database username: ');
+		$db_password    = readStdin('Database password: ');
 		
 		// set up database, add user / password
 		$user_password = substr(md5($system_salt . 'password'),4,7);
@@ -114,34 +107,13 @@ echo "\n                       :+#\n"
 		}
 
 		if ($pdo->query(file_get_contents(dirname(__FILE__) . '/../../framework/php/settings/sql/cashmusic_db.sql'))) {
-			$password_hash = hash_hmac('sha256', $user_password, $system_salt);
-			$data = array(
-				'email_address' => $user_email,
-				'password'      => $password_hash,
-				'is_admin'      => true,
-				'creation_date' => time()
-			);
-			$query = "INSERT INTO people (email_address,password,is_admin,creation_date) VALUES (:email_address,:password,:is_admin,:creation_date)";
-
-			try {  
-				$q = $pdo->prepare($query);
-				$success = $q->execute($data);
-				if (!$success) {
-					echo "\nOh. Shit. Something's wrong. Couldn't add the user to the database.\n\n";
-					die();
-					break;
-				}
-			} catch(PDOException $e) {  
-				echo "\nOh. Shit. Something's wrong. Couldn't add the user to the database. $e\n\n";
-				die();
-				break;
-			}
+			$success = true;
 		} else {
 			echo "\nOh. Shit. Something's wrong. Couldn't create database tables.\n\n";
 			die();
 			break;
 		}
-	} else if ($db_engine == "sqlite") {
+	} else if ($db_engine == 'sqlite') {
 		$installer_root = dirname(__FILE__);
 
 		// if the file exists already, rename it as a backup
@@ -153,34 +125,41 @@ echo "\n                       :+#\n"
 				mkdir($installer_root . '/../../framework/php/db');
 			}
 		}
+		chmod($installer_root . '/../../framework/db',0755);
 		
 		// connect to the new db...will create if not found
 		try {
 			$pdo = new PDO ('sqlite:' . $installer_root . '/../../framework/db/cashmusic.sqlite');
 			$pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+			chmod($installer_root . '/../../framework/db/cashmusic.sqlite',0755);
 		} catch (PDOException $e) {
 			echo "\nOh. Shit. Something's wrong: Couldn't connect to the database. $e\n\n";
 			die();
 			break;
 		}
 
-		if ($pdo) {
-			chmod($installer_root . '/../../framework/db',0777);
-			chmod($installer_root . '/../../framework/db/cashmusic.sqlite',0777);
-		}
-
 		// push in all the tables
 		try {
 			$pdo->exec(file_get_contents($installer_root . '/../../framework/php/settings/sql/cashmusic_db_sqlite.sql'));
+			$success = true;
 		} catch (PDOException $e) {
 			echo "\nOh. Shit. Something's wrong: Couldn't write to the database. $e\n\n";
 			die();
 			break;
 		}
+	}
 
-		$user_email    = readStdin("\nMain system login email address: ");
-		$system_salt   = md5($user_email . time());
-		$user_password = substr(md5($system_salt . 'password'),4,7);
+	if ($success) {
+		$user_email     = readStdin("\nLogin email address: ");
+		$user_password  = readStdin("Login password: ");
+		$default_salt   = readStdin("\nUse defaut system salt? (y/n): ", false, 'y');
+		
+		if (strtolower(substr($default_salt,0,1)) == 'y') {
+			$system_salt = 'I was born of sun beams; Warming up our limbs';
+		} else {
+			$system_salt = md5($user_email . time());
+		}
+
 		$password_hash = hash_hmac('sha256', $user_password, $system_salt);
 	
 		$data = array(
@@ -193,24 +172,17 @@ echo "\n                       :+#\n"
 		);
 		$query = "INSERT INTO people (email_address,password,is_admin,api_key,api_secret,creation_date) VALUES (:email_address,:password,:is_admin,:api_key,:api_secret,:creation_date)";
 		
-		try {
+		try {  
 			$q = $pdo->prepare($query);
-		} catch (PDOException $e) {
-			echo "\nOh. Shit. Something's wrong: Couldn't prepare query. $e\n\n";
-			die();
-			break;
-		}
-		
-		try {
 			$success = $q->execute($data);
-		} catch(PDOException $e) {
+		} catch(PDOException $e) {  
 			echo "\nOh. Shit. Something's wrong. Couldn't add the user to the database. $e\n\n";
 			die();
 			break;
 		}
-	}
 
-	if ($success) {
+
+
 		$installer_root = dirname(__FILE__);
 		// modify settings files
 		if (
