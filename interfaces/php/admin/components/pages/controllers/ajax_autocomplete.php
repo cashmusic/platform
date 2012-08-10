fb0bc15771a70f88542d66fbd209511a77b0dfc0
Jesse von Doom <jesse@cashmusic.org>
2012-08-10 12:13:42 -0700

    autocomplete stub

---
+++ interfaces/php/admin/components/pages/controllers/ajax_autocomplete.php
@@ -0,0 +1,29 @@
+[
+	{
+		"id":"2345",
+		"displayString":"word"
+	},
+	{
+		"id":"23456",
+		"displayString":"another word"
+	},
+	{
+		"id":"234567",
+		"displayString":"another world"
+	},
+	{
+		"id":"2345678",
+		"displayString":"wait? the one with denise?"
+	},
+	{
+		"id":"23456789",
+		"displayString":"yeah marisa thome was on that shit"
+	},
+	{
+		"id":"234567890",
+		"displayString":"word"
+	}
+]
+<?php
+	exit();
+?>
