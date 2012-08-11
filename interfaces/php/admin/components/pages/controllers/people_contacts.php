c3f236ac07cd549fdb3cf8fc0f72f0b57023442f
Jesse von Doom <jesse@cashmusic.org>
2012-08-11 15:36:19 -0700

    added swift to the licenses page, took out my stupid SMTP mailer test

--- interfaces/php/admin/components/pages/controllers/people_contacts.php
+++ interfaces/php/admin/components/pages/controllers/people_contacts.php
@@ -1,13 +1,5 @@
 <?php
 $cash_admin->page_data['country_codes'] = AdminHelper::drawCountryCodeUL();
 
-CASHSystem::sendEmail(
-	'Hi there.',
-	CASHSystem::getDefaultEmail(),
-	'jessevondoom@gmail.com',
-	'This is just a stupid fucking test, but you know that already.',
-	'Hello'
-);
-
 $cash_admin->setPageContentTemplate('people_contacts');
 ?>
