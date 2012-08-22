fcf5072e4392ebda948f1b5200c321d6daf63b31
Jesse von Doom <jesse@cashmusic.org>
2012-08-22 14:53:01 -0700

    fixed a minor bug in handling lightboxed drawers

--- interfaces/php/admin/ui/default/assets/scripts/jquery.admin.js
+++ interfaces/php/admin/ui/default/assets/scripts/jquery.admin.js
@@ -442,37 +442,39 @@
 			// minimize jQuery calls and simplify. set each element up fron in the function scope:
 			var drawer, drawerHandle, drawerContent, drawerHandleLabel;
 			drawer = $(this);
-			drawerHandle = drawer.find('.drawerhandle');
-			drawerContent = drawer.find('.drawercontent');
-			// create the label span and add necessary classes
-			drawerHandleLabel = $('<span class="drawerhandleaction">' + labelTextHidden + ' </span>');
-			if (labelClassVisible) {
-				drawerHandleLabel.addClass(labelClassHidden);
-			}
-			// first hide the content add a label to all the drawerhandles
-			drawerContent.hide();
-			drawerHandle.prepend(drawerHandleLabel);
-			// then set up click actions on each of them
-			$(this).find('.drawerhandle').on('click',function () {
-				if (drawerContent.is(':hidden')) {
-					drawerContent.slideDown(200, function () {
-						drawerHandleLabel.html(labelTextVisible + ' ');
-						if (labelClassVisible) {
-							drawerHandleLabel.removeClass();
-							drawerHandleLabel.addClass(labelClassVisible);
-						}
-					});
-				} else {
-					drawerContent.slideUp(200, function () {
-		    			drawerContent.hide();
-		    			drawerHandleLabel.html(labelTextHidden + ' ');
-		    			if (labelClassHidden) {
-			    			drawerHandleLabel.removeClass();
-			    			drawerHandleLabel.addClass(labelClassHidden);
-			    		}
-		    		});
+			if (drawer.find('.drawerhandleaction').length == 0) {
+				drawerHandle = drawer.find('.drawerhandle');
+				drawerContent = drawer.find('.drawercontent');
+				// create the label span and add necessary classes
+				drawerHandleLabel = $('<span class="drawerhandleaction">' + labelTextHidden + ' </span>');
+				if (labelClassVisible) {
+					drawerHandleLabel.addClass(labelClassHidden);
 				}
-			});
+				// first hide the content add a label to all the drawerhandles
+				drawerContent.hide();
+				drawerHandle.prepend(drawerHandleLabel);
+				// then set up click actions on each of them
+				$(this).find('.drawerhandle').on('click',function () {
+					if (drawerContent.is(':hidden')) {
+						drawerContent.slideDown(200, function () {
+							drawerHandleLabel.html(labelTextVisible + ' ');
+							if (labelClassVisible) {
+								drawerHandleLabel.removeClass();
+								drawerHandleLabel.addClass(labelClassVisible);
+							}
+						});
+					} else {
+						drawerContent.slideUp(200, function () {
+			    			drawerContent.hide();
+			    			drawerHandleLabel.html(labelTextHidden + ' ');
+			    			if (labelClassHidden) {
+				    			drawerHandleLabel.removeClass();
+				    			drawerHandleLabel.addClass(labelClassHidden);
+				    		}
+			    		});
+					}
+				});
+			}
 		});
 
 	}
