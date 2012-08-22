2e2207e94d1a100469c0feb9a62ed371a93369f1
Jesse von Doom <jesse@cashmusic.org>
2012-08-22 15:32:44 -0700

    fixed /ajax/initiateupload to reuqire a connection id as its first parameter

--- interfaces/php/admin/components/pages/controllers/ajax_initiateupload.php
+++ interfaces/php/admin/components/pages/controllers/ajax_initiateupload.php
@@ -1,13 +1,21 @@
 <?php
-$upload_response = $cash_admin->requestAndStore(
-	array(
-		'cash_request_type' => 'asset', 
-		'cash_action' => 'addremoteuploadform',
-		'user_id' => AdminHelper::getPersistentData('cash_effective_user'),
-		'connection_id' => 1
-	)
-);
-
-echo json_encode($upload_response['payload']);
+if (!isset($request_parameters[0])) {
+	echo json_encode(array("success" => false));
+} else {
+	$upload_response = $cash_admin->requestAndStore(
+		array(
+			'cash_request_type' => 'asset', 
+			'cash_action' => 'addremoteuploadform',
+			'user_id' => AdminHelper::getPersistentData('cash_effective_user'),
+			'connection_id' => $request_parameters[0]
+		)
+	);
+	if ($upload_response['payload']) {
+		$upload_response['payload']['success'] = true;
+		echo json_encode($upload_response['payload']);
+	} else {
+		echo json_encode(array("success" => false));
+	}
+}
 exit();
 ?>
