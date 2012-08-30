9471275429c530fcf95ef7a6d80e1c041b10fdc3
Jesse von Doom <jesse@cashmusic.org>
2012-08-29 18:36:03 -0700

    Asset delete controller - Fixes #200

---
+++ interfaces/php/admin/components/pages/controllers/assets_delete.php
@@ -0,0 +1,25 @@
+<?php
+if (!$request_parameters) {
+	AdminHelper::controllerRedirect('/assets/');
+}
+
+
+if (isset($_POST['dodelete']) || isset($_REQUEST['modalconfirm'])) {
+	$asset_delete_request = new CASHRequest(
+		array(
+			'cash_request_type' => 'asset', 
+			'cash_action' => 'deleteasset',
+			'id' => $request_parameters[0]
+		)
+	);
+	if ($asset_delete_request->response['status_uid'] == 'asset_deleteasset_200') {
+		if (isset($_REQUEST['redirectto'])) {
+			AdminHelper::formSuccess('Success. Deleted.',$_REQUEST['redirectto']);
+		} else {
+			AdminHelper::formSuccess('Success. Deleted.','/assets/');
+		}
+	}
+}
+
+$cash_admin->setPageContentTemplate('delete_confirm');
+?>
