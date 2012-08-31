7be656743b5b1d2c92446593e0e1dcb4ff1d7af9
Jesse von Doom <jesse@cashmusic.org>
2012-08-30 21:04:00 -0700

    orders listed on main commerce page - Fixes #184

--- interfaces/php/admin/components/pages/controllers/commerce.php
+++ interfaces/php/admin/components/pages/controllers/commerce.php
@@ -28,7 +28,39 @@ if (is_array($items_response['payload'])) {
 }
 
 if (is_array($orders_response['payload'])) {
-	$cash_admin->page_data['orders_all'] = new ArrayIterator($items_response['payload']);
+	$all_order_details = array();
+	foreach ($orders_response['payload'] as $order) {
+		if ($order['canceled'] == 0) {
+			
+			$order_details_request = new CASHRequest(
+				array(
+					'cash_request_type' => 'commerce', 
+					'cash_action' => 'getorder',
+					'id' => $order['id'],
+					'deep' => true
+				)
+			);
+			
+			$order_details = $order_details_request->response['payload'];
+			if ($order_details['successful']) {
+				$order_date = $order_details['creation_date'];
+				if ($order_details['creation_date']) {
+					$order_date = $order_details['modification_date'];
+				}
+				
+				$all_order_details[] = array(
+					'id' => $order_details['id'],
+					'number' => '#' . str_pad($order_details['id'],6,0,STR_PAD_LEFT),
+					'date' => CASHSystem::formatTimeAgo((int)$order_date),
+					'items' => str_replace('\n','<br />',$order_details['order_totals']['description']),
+					'gross' => '$' . sprintf("%01.2f",$order_details['gross_price']),
+				);
+			}
+		}
+	}
+	if (count($all_order_details) > 0) {
+		$cash_admin->page_data['orders_recent'] = new ArrayIterator($all_order_details);
+	}
 }
 
 $cash_admin->setPageContentTemplate('commerce');
