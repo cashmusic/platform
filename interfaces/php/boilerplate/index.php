35ae6ad54b09a84bfac71fdb56073ed209662a52
Jesse von Doom <jesse@cashmusic.org>
2012-09-02 11:12:41 -0700

    adding an "interfaces/php/boilerplate" that will install with the platform. starting point for quick playing...

---
+++ interfaces/php/boilerplate/index.php
@@ -0,0 +1,16 @@
+<?php // REPLACE WITH INIT CODE FROM ADMIN > SYSTEM SETTINGS // Initialize CASH Music ?>
+<!DOCTYPE html>
+<head> 
+<title>Boilerplate / CASH Music</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="assets/css/cashmusic.css" rel="stylesheet" type="text/css" />
+<link rel="icon" type="image/png" href="http://cashmusic.org/images/icons/cash.png" />
+</head> 
+<body>
+
+	<div id="mainspc">
+		<?php // REPLACE WITH ANY ELEMENT PHP EMBED // Element ?>
+	</div>
+
+</body> 
+</html>
