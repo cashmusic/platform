99d5e023ab3c4e99f32c62590d787a5c644c0231
Jesse von Doom <jesse@cashmusic.org>
2012-10-18 10:23:11 -0700

    utf8 encoding specified in admin forms

--- interfaces/php/admin/components/pages/controllers/settings_update.php
+++ interfaces/php/admin/components/pages/controllers/settings_update.php
@@ -1,6 +1,6 @@
 <?php
 if (isset($_POST['doupgrade'])) {
-	$upgrade_file_contents = CASHSystem::getURLContents('https://raw.github.com/cashmusic/DIY/latest_stable/installers/php/update/update.php');
+	$upgrade_file_contents = CASHSystem::getURLContents('https://raw.github.com/cashmusic/platform/latest_stable/installers/php/update/update.php');
 	if ($upgrade_file_contents) {
 		$success = file_put_contents(ADMIN_BASE_PATH . '/../update.php', $upgrade_file_contents);
 		if ($success) {
@@ -16,12 +16,12 @@ if (isset($_POST['doupgrade'])) {
 $cash_admin->page_data['platform_version'] = CASHRequest::$version;
 $cash_admin->page_data['upgrade_available'] = false;
 
-$current_profile_url = 'https://raw.github.com/cashmusic/DIY/latest_stable/installers/php/update/releaseprofiles/release_'
+$current_profile_url = 'https://raw.github.com/cashmusic/platform/latest_stable/installers/php/update/releaseprofiles/release_'
 					 . $cash_admin->page_data['platform_version']
 					 . '.json';
 if (CASHSystem::getURLContents($current_profile_url)) {
 	// found stable upgrade path...neat! let's check for a proper upgrade script:
-	$upgrade_script_url = 'https://raw.github.com/cashmusic/DIY/latest_stable/installers/php/update/updatescripts/'
+	$upgrade_script_url = 'https://raw.github.com/cashmusic/platform/latest_stable/installers/php/update/updatescripts/'
 						. $cash_admin->page_data['platform_version']
 						. '.php';
 	$upgrade_script_contents = CASHSystem::getURLContents($upgrade_script_url);
