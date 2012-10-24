dc5d799838522b1dceb229a4613a9844955a3f44
Jesse von Doom <jesse@cashmusic.org>
2012-10-24 15:19:29 -0700

    hotfix for 4->5 upgrade. forgot to add sessions table

--- installers/php/update/updatescripts/4.php
+++ installers/php/update/updatescripts/4.php
@@ -16,6 +16,107 @@
 $upgrade_failure = false;
 
 if ($current_version == 4) {
+	include_once(dirname(__FILE__) . '/../../../../../admin/constants.php');
+
+	/***************************
+	 *
+	 * 0. DEFINITIONS
+	 *
+	 ***************************/
+	$cash_settings = parse_ini_file(CASH_PLATFORM_ROOT.'/settings/cashmusic.ini.php');
+	$cash_settings['port'] = '';
+	if (strpos($cash_settings['hostname'],':') === false) {
+		$cash_settings['port'] = 3306;
+	} else {
+		if (!substr($hostname,0,2) == ':/') {
+			$host_and_port = explode(':',$hostname);
+			$cash_settings['hostname'] = $host_and_port[0];
+			$cash_settings['port'] = $host_and_port[1];
+		}
+	}
+
+	/***************************
+	 *
+	 * 1. DO SCHEMA CHANGES
+	 *
+	 ***************************/
+
+	try {  
+		if ($cash_settings['driver'] == 'sqlite') {
+			@copy(CASH_PLATFORM_ROOT . "/../db/{$cash_settings['database']}",CASH_PLATFORM_ROOT . "/../db/{$cash_settings['database']}.bak");
+      		chmod(CASH_PLATFORM_ROOT . "/../db/{$cash_settings['database']}.bak",0755);
+			$pdo = new PDO("sqlite:" . CASH_PLATFORM_ROOT . "/../db/{$cash_settings['database']}");
+		} else {
+			if (substr($cash_settings['hostname'],0,2) == ':/') {
+				$pdo = new PDO("{$cash_settings['driver']}:unix_socket={$cash_settings['hostname']};dbname={$cash_settings['database']}", $cash_settings['username'], $cash_settings['password']);
+			} else {
+				$pdo = new PDO("{$cash_settings['driver']}:host={$cash_settings['hostname']};port={$cash_settings['port']};dbname={$cash_settings['database']}", $cash_settings['username'], $cash_settings['password']);
+			}
+		}
+		$pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+	} catch(PDOException $e) {  
+		$upgrade_failure = true;
+	}
+
+	if (!$upgrade_failure) {
+		if ($cash_settings['driver'] == 'sqlite') {
+			$query = <<<SQLITE
+BEGIN TRANSACTION;
+
+CREATE TABLE system_sessions (
+  id INTEGER PRIMARY KEY,
+  session_id text,
+  data text,
+  client_ip text,
+  client_proxy text,
+  expiration_date integer,
+  creation_date integer DEFAULT NULL,
+  modification_date integer DEFAULT NULL
+);
+CREATE INDEX system_sessions_session_id ON system_sessions (session_id);
+CREATE INDEX system_sessions_expiration_date ON system_sessions (expiration_date);
+
+COMMIT;
+SQLITE;
+		} else {
+			$query = <<<MYSQL
+ALTER TABLE `people` 
+  CHANGE `password` `password` varchar(255) NOT NULL DEFAULT '';
+
+DROP TABLE IF EXISTS `system_sessions`;
+CREATE TABLE `system_sessions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `session_id` varchar(255) NOT NULL,
+  `data` text NOT NULL,
+  `client_ip` varchar(39),
+  `client_proxy` varchar(39),
+  `expiration_date` int(11),
+  `creation_date` int(11) DEFAULT NULL,
+  `modification_date` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `system_sessions_session_id` (`session_id`),
+  KEY `system_sessions_expiration_date` (`expiration_date`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+MYSQL;
+		}
+	}
+
+	try {
+		$pdo->exec($query);
+	} catch (PDOException $e) {
+		if ($cash_settings['driver'] == 'sqlite') {
+			unlink(CASH_PLATFORM_ROOT . "/../db/{$cash_settings['database']}");
+			@rename(CASH_PLATFORM_ROOT . "/../db/{$cash_settings['database']}.bak",CASH_PLATFORM_ROOT . "/../db/{$cash_settings['database']}");
+	    }
+		$upgrade_failure = true;
+	}
+
+	/***************************
+	 *
+	 * 1. MODIFY NEW FILES
+	 *
+	 ***************************/
+
 	$api_dir = rtrim(dirname($_SERVER['REQUEST_URI']),'/') . '/api';
 	$public_dir = rtrim(dirname($_SERVER['REQUEST_URI']),'/') . '/public';
 
