8457ad8be8e67a0bda0435470f4534eea25f8966
Jesse von Doom <jesse@cashmusic.org>
2012-12-03 12:36:34 -0800

    better account management page

--- interfaces/php/admin/components/pages/controllers/account.php
+++ interfaces/php/admin/components/pages/controllers/account.php
@@ -1,41 +1,47 @@
 <?php
-function checkLogin() {
-	if (isset($_POST['email_address']) && isset($_POST['password'])) {
-		$login_details = AdminHelper::doLogin($_POST['email_address'],$_POST['password']);
-		if ($login_details !== false) {
-			return true;
-		} else {
-			return false;
-		}
-	} else {
-		return false;
-	}
-}
+if (isset($_POST['doaccountchange'])) {
+	$valid_user_response = $cash_admin->requestAndStore(
+		array(
+			'cash_request_type' => 'system', 
+			'cash_action' => 'validatelogin',
+			'address' => $_POST['email_address'],
+			'password' => $_POST['password'],
+			'require_admin' => true,
+			'keep_session' => true
+		)
+	);
 
-if (isset($_POST['doemailchange']) || isset($_POST['dopasswordchange'])) {
-	if (!checkLogin()) {
-		$cash_admin->page_data['error_message'] = 'Error. There was a problem with your password. Please try again.';
+	if (!$valid_user_response['payload']) {
+		AdminHelper::formFailure('Error. There was a problem with your password. Please try again.');
 	} else {
-		$email_address = $_POST['email_address'];
-		$password = $_POST['password'];
-		if (isset($_POST['new_email_address'])) { $email_address = $_POST['new_email_address']; }
-		if (isset($_POST['new_password'])) { $password = $_POST['new_password']; }
-		$login_request = new CASHRequest(
-			array(
-				'cash_request_type' => 'system', 
-				'cash_action' => 'setlogincredentials',
-				'user_id' => $cash_admin->effective_user_id, 
-				'address' => $email_address, 
-				'password' => $password
-			)
+		$changes = array(
+			'cash_request_type' => 'system', 
+			'cash_action' => 'setlogincredentials',
+			'user_id' => $cash_admin->effective_user_id
 		);
-		if ($login_request->response['payload'] !== false) {
-			if (isset($_POST['doemailchange'])) {
-				$admin_primary_cash_request->sessionSet('cash_effective_user_email',$email_address);
+		if (isset($_POST['new_email_address'])) { 
+			if ($_POST['new_email_address']) {
+				$changes['address'] = $_POST['new_email_address'];
 			}
-			$cash_admin->page_data['page_message'] = 'Success. All changed.';
+		}
+		if (isset($_POST['new_username'])) { 
+			if ($_POST['new_username']) {
+				$changes['username'] = $_POST['new_username'];
+			}
+		}
+		if (isset($_POST['new_password'])) { 
+			if ($_POST['new_password']) {
+				$changes['password'] = $_POST['new_password'];
+			}
+		}
+		$change_response = $cash_admin->requestAndStore($changes);
+		if ($change_response['payload'] !== false) {
+			if (isset($changes['address'])) {
+				$admin_primary_cash_request->sessionSet('cash_effective_user_email',$changes['address']);
+			}
+			AdminHelper::formSuccess('Success. All changed.');
 		} else {
-			$cash_admin->page_data['error_message'] = 'Error. We had a problem resetting your login. Please try again.';
+			AdminHelper::formFailure('Error. We had a problem resetting your login. Please try again.');
 		}
 	}
 }
@@ -51,6 +57,7 @@ $user_request = $cash_admin->requestAndStore(
 
 if (is_array($user_request['payload'])) {
 	$cash_admin->page_data['email_address'] = $user_request['payload']['email_address'];
+	$cash_admin->page_data['username'] = $user_request['payload']['username'];
 	$cash_admin->page_data['api_key'] = $user_request['payload']['api_key'];
 	$cash_admin->page_data['api_url'] = CASH_API_URL;
 	if (isset($_REQUEST['reveal'])) {
