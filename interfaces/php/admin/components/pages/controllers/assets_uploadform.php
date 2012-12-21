523d8a6907993bcf0e03397f5d7e74f207f2bd78
Jesse von Doom <jesse@cashmusic.org>
2012-12-21 14:51:40 -0800

    make the upload pages understand different types

--- interfaces/php/admin/components/pages/controllers/assets_uploadform.php
+++ interfaces/php/admin/components/pages/controllers/assets_uploadform.php
@@ -1,16 +1,19 @@
 <?php
+$cash_admin->setPageContentTemplate('error');
 if (isset($request_parameters[0])) {
 	$param_response = $cash_admin->requestAndStore(
 		array(
 			'cash_request_type' => 'asset', 
 			'cash_action' => 'getuploadparameters',
-			'connection_id' => $request_parameters[0]
+			'connection_id' => $request_parameters[0],
+			'user_id' => $cash_admin->effective_user_id
 		)
 	);
 	if (is_array($param_response['payload'])) {
 		$cash_admin->page_data = array_merge($cash_admin->page_data,$param_response['payload']);
 		$cash_admin->page_data['connection_id'] = $request_parameters[0];
+		$which_form = str_replace('.','',$param_response['payload']['connection_type']);
+		$cash_admin->setPageContentTemplate('assets_uploadform_' . $which_form);
 	}
 }
-$cash_admin->setPageContentTemplate('assets_uploadform');
 ?>
