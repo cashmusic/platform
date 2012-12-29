600cbbee39577d834006315760c56b06637355e8
Jesse von Doom <jesse@cashmusic.org>
2012-12-28 17:15:59 -0800

    single/multi flag in help topics - Fixes #235

--- interfaces/php/admin/components/pages/controllers/help.php
+++ interfaces/php/admin/components/pages/controllers/help.php
@@ -1,8 +1,19 @@
 <?php
 	$help_topics = json_decode(file_get_contents(ADMIN_BASE_PATH . '/components/text/en/help/topics.json'),true);
 	
+	$filtered_topics = array();
+	foreach ($help_topics as $topic) {
+		// check instance-type compatibility
+		if (in_array($cash_admin->platform_type, $topic['compatibility'])) {
+			$filtered_topics[] = array(
+				'file' => $topic['file'],
+				'title' => $topic['title']
+			);
+		}
+	}
+
 	if (!isset($request_parameters[0])) {
-		$cash_admin->page_data['help_topics'] = new ArrayIterator($help_topics);
+		$cash_admin->page_data['help_topics'] = new ArrayIterator($filtered_topics);
 		$cash_admin->setPageContentTemplate('help_topics');
 	} else {
 		if (file_exists(CASH_PLATFORM_ROOT . '/lib/markdown/markdown.php')) {
