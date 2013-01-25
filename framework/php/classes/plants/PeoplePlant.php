22211449e115ffd8a8b5b270b932e8513e9e9218
Jesse von Doom <jesse@cashmusic.org>
2013-01-24 22:40:19 -0800

    moving the core to LGPL, updating copyright and licensing details

--- framework/php/classes/plants/PeoplePlant.php
+++ framework/php/classes/plants/PeoplePlant.php
@@ -7,9 +7,9 @@
  * @author CASH Music
  * @link http://cashmusic.org/
  *
- * Copyright (c) 2012, CASH Music
- * Licensed under the Affero General Public License version 3.
- * See http://www.gnu.org/licenses/agpl-3.0.html
+ * Copyright (c) 2013, CASH Music
+ * Licensed under the GNU Lesser General Public License version 3.
+ * See http://www.gnu.org/licenses/lgpl-3.0.html
  *
  * violet           hope
  *
--- framework/php/classes/seeds/PaypalSeed.php
+++ framework/php/classes/seeds/PaypalSeed.php
@@ -10,9 +10,9 @@
  * original script(s) here:
  * https://cms.paypal.com/us/cgi-bin/?cmd=_render-content&content_ID=developer/library_download_sdks#NVP
  *
- * Copyright (c) 2012, CASH Music
- * Licensed under the Affero General Public License version 3.
- * See http://www.gnu.org/licenses/agpl-3.0.html
+ * Copyright (c) 2013, CASH Music
+ * Licensed under the GNU Lesser General Public License version 3.
+ * See http://www.gnu.org/licenses/lgpl-3.0.html
  *
  **/
 class PaypalSeed extends SeedBase {
