e00b12173b2d37c28f57e10f6381771fb99994a0
Jesse von Doom <jesse@cashmusic.org>
2014-06-12 23:36:02 -0700

    spinning a physical preorder element out of digital purchase

---
+++ framework/php/elements/PhysicalPreorder.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Physical Preorder element
+ *
+ * @package digitalpurchase.org.cashmusic
+ * @author CASH Music
+ * @link http://cashmusic.org/
+ *
+ * Copyright (c) 2014, CASH Music
+ * Licensed under the Affero General Public License version 3.
+ * See http://www.gnu.org/licenses/agpl-3.0.html
+ *
+ **/
+class DigitalPurchase extends ElementBase {
+	public $type = 'physicalpreorder';
+	public $name = 'Physical Preorder';
+
+	public function getData() {
+		// define $markup to store all screen output
+		$item_request = new CASHRequest(
+			array(
+				'cash_request_type' => 'commerce', 
+				'cash_action' => 'getitem',
+				'id' => $this->options['item_id']
+			)
+		);
+		$item = $item_request->response['payload'];
+		$this->element_data['item_name'] = $item['name'];
+		$this->element_data['item_price'] = number_format($item['price'], 2, '.', '');
+		$this->element_data['item_flexible_price'] = $item['flexible_price'];
+		$this->element_data['item_description'] = $item['description'];
+		$this->element_data['item_asset'] = $item['fulfillment_asset'];
+
+		$currency_request = new CASHRequest(
+			array(
+				'cash_request_type' => 'system', 
+				'cash_action' => 'getsettings',
+				'type' => 'use_currency',
+				'user_id' => $this->element_data['user_id']
+			)
+		);
+		if ($currency_request->response['payload']) {
+			$this->element_data['currency'] = CASHSystem::getCurrencySymbol($currency_request->response['payload']);
+		} else {
+			$this->element_data['currency'] = CASHSystem::getCurrencySymbol('USD');
+		}
+
+		if (
+			$this->status_uid == 'commerce_finalizepayment_200' || 
+			$this->status_uid == 'element_redeemcode_200' ||
+			$this->status_uid == 'commerce_initiatecheckout_200' && $this->original_response['payload'] == 'force_success'
+			) {
+			if ($item['fulfillment_asset'] != 0) {
+				$fulfillment_request = new CASHRequest(
+					array(
+						'cash_request_type' => 'asset', 
+						'cash_action' => 'getfulfillmentassets',
+						'asset_details' => $item['fulfillment_asset']
+					)
+				);
+				if ($fulfillment_request->response['payload']) {
+					$this->element_data['fulfillment_assets'] = new ArrayIterator($fulfillment_request->response['payload']);
+				}
+			}
+			$this->setTemplate('success');
+		} elseif ($this->status_uid == 'commerce_initiatecheckout_400') {
+			// could happen on a database glitch, but probably means the user set a pay-minimum price below the
+			// minimum price. what a heel.
+			$this->element_data['error_message'] = 'Make sure you enter a price of at least ' . $this->element_data['currency'] . $item['price'] . ' and try again.';
+		} elseif ($this->status_uid == 'commerce_finalizepayment_400' || $this->status_uid == 'element_redeemcode_400') {
+			// payerid is specific to paypal, so this is temporary to tell between canceled and errored:
+			if (isset($_GET['PayerID'])) {
+				//$this->element_data['error_message'] = $this->options['message_error'];
+				$this->element_data['error_message'] = print_r($this->original_response,true);
+			}
+		}
+		return $this->element_data;	
+	}
+} // END class 
+?>
