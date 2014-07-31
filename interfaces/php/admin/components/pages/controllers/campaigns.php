cc02f6ddca58181a568a432d5c56cccfacc72570
Jesse von Doom <jesse@cashmusic.org>
2014-07-30 17:20:59 -0700

    moving campaigns to main page

--- interfaces/php/admin/components/pages/controllers/campaigns.php
+++ interfaces/php/admin/components/pages/controllers/campaigns.php
@@ -1,22 +1,3 @@
 <?php
-$campaigns_response = $cash_admin->requestAndStore(
-	array(
-		'cash_request_type' => 'element', 
-		'cash_action' => 'getcampaignsforuser',
-		'user_id' => $cash_admin->effective_user_id
-	)
-);
-
-if (is_array($campaigns_response['payload'])) {
-	foreach ($campaigns_response['payload'] as &$campaign) {
-		if ($campaign['modification_date'] == 0) {
-			$campaign['formatted_date'] = CASHSystem::formatTimeAgo($campaign['creation_date']);	
-		} else {
-			$campaign['formatted_date'] = CASHSystem::formatTimeAgo($campaign['modification_date']);
-		}
-	}
-	$cash_admin->page_data['campaigns_for_user'] = new ArrayIterator($campaigns_response['payload']);
-} 
-
-$cash_admin->setPageContentTemplate('campaigns');
+AdminHelper::controllerRedirect('/');
 ?>
