f54519441f4ad84ef929c6ee42426671771f02d8
Christopher Leckie <me@christopherleckie.com>
2015-02-02 14:31:45 -0800

    Adding Pure Grid CDN back in

--- interfaces/admin/constants.php
+++ interfaces/admin/constants.php
@@ -28,8 +28,8 @@ define('ALLOW_SIGNUPS',true); // should people be able to sign up from the admin
 define('CDN_URL','https://91ee07a61ca29df61569-b2dba7dce06e8a9c0977ad3a8844e9c8.ssl.cf2.rackcdn.com/v3'); // base CDN for some static assets (img tags, mostly)
 define('JQUERY_URL','//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'); // CDN for jQuery
 define('JQUERYUI_URL','//ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/jquery-ui.min.js'); // CDN for jQuery UI
-//define('PURE_BASE','//oss.maxcdn.com/pure/0.5.0/base-min.css'); // CDN for Pure Base
-//define('PURE_GRID','//oss.maxcdn.com/pure/0.5.0/grids-responsive-min.css'); // CDN for Pure Grid
+define('PURE_BASE','//oss.maxcdn.com/pure/0.5.0/base-min.css'); // CDN for Pure Base
+define('PURE_GRID','//oss.maxcdn.com/pure/0.5.0/grids-responsive-min.css'); // CDN for Pure Grid
 define('PURE_GRID_IE','//oss.maxcdn.com/pure/0.5.0/grids-responsive-old-ie-min.css'); // CDN for Pure Grid IE Fallback
 define('MINIMUM_PASSWORD_LENGTH',10); // doesn't allow passwords shorter than this value
 define('SUBDOMAIN_USERNAMES',true); // subdomain or subdirectory for user URL scheme (x.cashmusic.org v cashmuisc.org/x)
