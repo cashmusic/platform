fd53cb1d6bd3a5a43053a40264fe3ee0e2d70245
Jesse von Doom <jesse@cashmusic.org>
2015-03-11 21:55:17 -0700

    working order exports with filters and options

--- interfaces/admin/components/pages/controllers/commerce_orders_export.php
+++ interfaces/admin/components/pages/controllers/commerce_orders_export.php
@@ -1,62 +1,74 @@
 <?php
-// first grab first param — so we can later search for 'unfulfilled'
-$allorunfulfilled = $request_parameters[0];
+if (isset($_POST['export_options'])) {
 
-// initial details for the request
-$request_details = array(
-	'cash_request_type' => 'commerce', 
-	'cash_action' => 'getordersforuser',
-	'user_id' => $cash_admin->effective_user_id,
-	'deep' => 1
-);
+	// initial details for the request
+	$request_details = array(
+		'cash_request_type' => 'commerce', 
+		'cash_action' => 'getordersforuser',
+		'user_id' => $cash_admin->effective_user_id,
+		'deep' => 1
+	);
 
-// add unfulfilled_only if it applies
-if ($allorunfulfilled == 'unfulfilled') {
-	$request_details['unfulfilled_only'] = 1;
-} else {
-	$allorunfulfilled = 'all';
-}
+	if ($_POST['export_options'] == 'unfulfilled') {
+		$request_details['unfulfilled_only'] = 1;
+	}
 
-// make the request:
-$orders_response = $cash_admin->requestAndStore($request_details);
+	// make the request:
+	$orders_response = $cash_admin->requestAndStore($request_details);
 
-if (is_array($orders_response)) {
-	header('Content-Disposition: attachment; filename="orders_' . $allorunfulfilled . '_' . date('Mj-Y',time()) . '.csv"');
-	if ($orders_response['status_uid'] == 'commerce_getordersforuser_200') {
+	if (is_array($orders_response)) {
+		header('Content-Disposition: attachment; filename="orders_' . $_POST['export_options'] . '_' . date('Mj-Y',time()) . '.csv"');
 		echo '"order id","order date","description","shipping name","first name","last name","address 1","address 2","city","region","postal code","country code","country","gross price","service fee"' . "\n";
-		
-		foreach ($orders_response['payload'] as $entry) {
-		    echo '"' . str_replace ('"','""',$entry['id']) . '"';
-			echo ',"' . date('M j, Y h:iA T',$entry['modification_date']) . '"';
-			echo ',"' . str_replace ('"','""',$entry['transaction_description']) . '"';
-			echo ',"' . str_replace ('"','""',$entry['customer_shipping_name']) . '"';
-			echo ',"' . str_replace ('"','""',$entry['customer_first_name']) . '"';
-			echo ',"' . str_replace ('"','""',$entry['customer_last_name']) . '"';
-			echo ',"' . str_replace ('"','""',$entry['customer_address1']) . '"';
-			echo ',"' . str_replace ('"','""',$entry['customer_address2']) . '"';
-			echo ',"' . str_replace ('"','""',$entry['customer_city']) . '"';
-			echo ',"' . str_replace ('"','""',$entry['customer_region']) . '"';
-			echo ',"' . str_replace ('"','""',$entry['customer_postalcode']) . '"';
-			echo ',"' . str_replace ('"','""',$entry['customer_countrycode']) . '"';
-			echo ',"' . str_replace ('"','""',$entry['customer_country']) . '"';
-			echo ',"' . str_replace ('"','""',$entry['gross_price']) . '"';
-			echo ',"' . str_replace ('"','""',$entry['service_fee']) . '"';
-			echo "\n";
 
-			if ($allorunfulfilled == 'unfulfilled') {
-				// mark as fulfilled
-				$cash_admin->requestAndStore(
-					array(
-						'cash_request_type' => 'commerce', 
-						'cash_action' => 'editorder',
-						'id' => $entry['id'],
-						'fulfilled' => 1
-					)
-				);
+		if ($orders_response['status_uid'] == 'commerce_getordersforuser_200') {			
+			foreach ($orders_response['payload'] as $entry) {
+				$go = true;
+				if ($_POST['export_options'] == 'fulfilled') {
+					if (!$entry['fulfilled']) {
+						$go = false;
+					}
+				} elseif ($_POST['export_options'] == 'physical') {
+					if (!$entry['physical']) {
+						$go = false;
+					}
+				} elseif ($_POST['export_options'] == 'digital') {
+					if ($entry['physical'] || !$entry['digital']) {
+						$go = false;
+					}
+				} 
+
+				if ($go) {
+				    echo '"' . str_replace ('"','""',$entry['id']) . '"';
+					echo ',"' . date('M j, Y h:iA T',$entry['modification_date']) . '"';
+					echo ',"' . str_replace ('"','""',$entry['transaction_description']) . '"';
+					echo ',"' . str_replace ('"','""',$entry['customer_shipping_name']) . '"';
+					echo ',"' . str_replace ('"','""',$entry['customer_first_name']) . '"';
+					echo ',"' . str_replace ('"','""',$entry['customer_last_name']) . '"';
+					echo ',"' . str_replace ('"','""',$entry['customer_address1']) . '"';
+					echo ',"' . str_replace ('"','""',$entry['customer_address2']) . '"';
+					echo ',"' . str_replace ('"','""',$entry['customer_city']) . '"';
+					echo ',"' . str_replace ('"','""',$entry['customer_region']) . '"';
+					echo ',"' . str_replace ('"','""',$entry['customer_postalcode']) . '"';
+					echo ',"' . str_replace ('"','""',$entry['customer_countrycode']) . '"';
+					echo ',"' . str_replace ('"','""',$entry['customer_country']) . '"';
+					echo ',"' . str_replace ('"','""',$entry['gross_price']) . '"';
+					echo ',"' . str_replace ('"','""',$entry['service_fee']) . '"';
+					echo "\n";
+				}
+
+				if (isset($_POST['mark_fulfilled']) && $go) {
+					// mark as fulfilled
+					$cash_admin->requestAndStore(
+						array(
+							'cash_request_type' => 'commerce', 
+							'cash_action' => 'editorder',
+							'id' => $entry['id'],
+							'fulfilled' => 1
+						)
+					);
+				}
 			}
 		}
-	} else {
-		echo "There are no matching orders.";
 	}
 }
 
