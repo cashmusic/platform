fcf1bf643f860be367804e10d74c4e64ba455acf
Jesse von Doom <jesse@cashmusic.org>
2015-03-12 21:50:38 -0700

    deleting campaigns selects a new one, adding a campaign selects it

--- interfaces/admin/components/pages/controllers/campaigns_add.php
+++ interfaces/admin/components/pages/controllers/campaigns_add.php
@@ -13,6 +13,19 @@ if (isset($_POST['docampaignadd'])) {
 		)
 	);
 	if ($add_response['payload']) {
+		// make the new campaign selected
+		$admin_primary_cash_request->sessionSet('current_campaign',$add_response['payload']);
+		
+		$settings_request = new CASHRequest(
+			array(
+				'cash_request_type' => 'system', 
+				'cash_action' => 'setsettings',
+				'type' => 'selected_campaign',
+				'value' => $add_response['payload'],
+				'user_id' => $cash_admin->effective_user_id
+			)
+		);
+
 		AdminHelper::formSuccess('Success. Campaign added.','/');
 	} else {
 		AdminHelper::formFailure('Error. Something just didn\'t work right.','/campaigns/add/');
--- interfaces/admin/components/pages/controllers/campaigns_delete.php
+++ interfaces/admin/components/pages/controllers/campaigns_delete.php
@@ -12,6 +12,30 @@ if (isset($_POST['dodelete']) || isset($_REQUEST['modalconfirm'])) {
 		)
 	);
 	if ($delete_response['status_uid'] == 'element_deletecampaign_200') {
+		// get all campaigns 
+		$campaigns_response = $cash_admin->requestAndStore(
+			array(
+				'cash_request_type' => 'element', 
+				'cash_action' => 'getcampaignsforuser',
+				'user_id' => $cash_admin->effective_user_id
+			)
+		);
+		// if there's at least one remaining, select it
+		if (count($campaigns_response['payload'])) {
+			$current_campaign = $campaigns_response['payload'][count($campaigns_response['payload']) - 1]['id'];
+			$admin_primary_cash_request->sessionSet('current_campaign',$current_campaign);
+		
+			$settings_request = new CASHRequest(
+				array(
+					'cash_request_type' => 'system', 
+					'cash_action' => 'setsettings',
+					'type' => 'selected_campaign',
+					'value' => $current_campaign,
+					'user_id' => $cash_admin->effective_user_id
+				)
+			);
+		}
+
 		if (isset($_REQUEST['redirectto'])) {
 			AdminHelper::formSuccess('Success. Deleted.',$_REQUEST['redirectto']);
 		} else {
