8875aa57e08cff476994a0bed4ed4e9eb330cfd5
Jesse von Doom <jesse@cashmusic.org>
2015-04-15 15:09:07 -0700

    adding an (optional) ini-format settings override to the admin for repo-based deploys with custom settings

---
+++ interfaces/admin/admin_template.ini.php
@@ -0,0 +1,17 @@
+; <?php die(); __halt_compiler();
+;
+; Admin configuration file
+; Settings here override settings in the constants.php file, allowing you to set
+; them separately in a deploy that depends on the full platform repo.
+;
+; Copyright (c) 2015, CASH Music
+; Licensed under the Affero General Public License version 3.
+; See http://www.gnu.org/licenses/agpl-3.0.html
+
+[admin_options]
+allow_signups 				= false
+cdn_url						= "https://cashmusic.org/admin"
+jquery_url					= "//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"
+jqueryui_url 				= "//ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/jquery-ui.min.js"
+minimum_password_length = 10
+subdomain_usernames 		= true
--- interfaces/admin/constants.php
+++ interfaces/admin/constants.php
@@ -8,26 +8,37 @@ if (!file_exists($cashmusic_root)) {
 	$cash_settings = json_decode(getenv('cashmusic_platform_settings'),true);
 	// env settings allow use on multi-server, multi-user instances
 	if ($cash_settings) {
-		// thanks to json_decode this will be null if the 
+		// thanks to json_decode this will be null if the
 		if (isset($cash_settings['platforminitlocation'])) {
 			$cashmusic_root = $_SERVER['DOCUMENT_ROOT'] . $cash_settings['platforminitlocation'];
-		}	
-	} 
+		}
+	}
 }
 
-define('ADMIN_BASE_PATH', $root);
-define('ADMIN_WWW_BASE_PATH', '/admin');
-define('CASH_PLATFORM_PATH', $cashmusic_root);
+$settings = array(
+	'admin_base_path' => $root,
+	'admin_www_base_path' => '/admin',
+	'cash_platform_path' => $cashmusic_root
+);
+
 /*********************************************************
  *
  * OPTIONAL SETTINGS
- * (un-comment to set, otherwise defaults will be used.)
  *
  *********************************************************/
-define('ALLOW_SIGNUPS',true); // should people be able to sign up from the admin page?
-define('CDN_URL','https://cashmusic.org/admin'); // base CDN for some static assets (img tags, mostly)
-define('JQUERY_URL','//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'); // CDN for jQuery
-define('JQUERYUI_URL','//ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/jquery-ui.min.js'); // CDN for jQuery UI
-define('MINIMUM_PASSWORD_LENGTH',10); // doesn't allow passwords shorter than this value
-define('SUBDOMAIN_USERNAMES',true); // subdomain or subdirectory for user URL scheme (x.cashmusic.org v cashmuisc.org/x)
+$settings['allow_signups'] = true; // should people be able to sign up from the admin page?
+$settings['cdn_url'] = 'https://cashmusic.org/admin'; // base CDN for some static assets (img tags, mostly)
+$settings['jquery_url'] = '//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'; // CDN for jQuery
+$settings['jqueryui_url'] = '//ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/jquery-ui.min.js'; // CDN for jQuery UI
+$settings['minimum_password_length'] = 10; // doesn't allow passwords shorter than this value
+$settings['subdomain_usernames'] = true; // subdomain or subdirectory for user URL scheme (x.cashmusic.org v cashmuisc.org/x)
+
+// finally look for admin.ini.php for custom settings. if it exists pull it in.
+if (file_exists(__DIR__.'/admin.ini.php')) {
+	$settings = array_merge($settings,parse_ini_file(__DIR__.'/admin.ini.php'));
+}
+
+foreach ($settings as $key => $value) {
+	define(strtoupper($key),$value);
+}
 ?>
