dc5a74c3efeafcc11f4212fda3c0e923b1135043
Jesse von Doom <jesse@cashmusic.org>
2016-02-24 16:45:17 -0800

    streamlined SinglePurchase.php

--- framework/elements/SinglePurchase.php
+++ framework/elements/SinglePurchase.php
@@ -32,6 +32,23 @@ class SinglePurchase extends ElementBase {
 		$this->element_data['item_description'] = $item['description'];
 		$this->element_data['item_asset'] = $item['fulfillment_asset'];
 
+		// shipping
+		if ($item['physical_fulfillment'] == 1) {
+			error_log('hmm');
+			// according to the item, we've got a product with physical fulfillment--- let's assume it's shippable
+			// and revert to no shipping if it doesn't meet the basic requirements on shipping regions
+			$this->element_data['no_shipping'] = false;
+
+			/** LEGACY NOTICE: shipping has moved from elements to items. */
+			if ($item['shipping']) {
+				// we've got shipping set via the new item standard, so let's give them precedence over the legacy values
+				$this->element_data['region1_cost'] = $item['shipping']['r1-1'];
+				$this->element_data['region2_cost'] = $item['shipping']['r2-1'];
+			}
+		} else {
+			$this->element_data['no_shipping'] = true;
+		}
+
 		// item variants...ugh
 		if ($item['variants']) {
 			$this->element_data['has_variants'] = true;
@@ -123,10 +140,12 @@ class SinglePurchase extends ElementBase {
 			if ($settings_request->response['payload']['pp_default'] || $settings_request->response['payload']['pp_micro']) {
 				$this->element_data['paypal_connection'] = true;
 			}
-			if ($settings_request->response['payload']['stripe_default']) {
-				$payment_seed = new StripeSeed($this->element_data['user_id'],$settings_request->response['payload']['stripe_default']);
-				if (!empty($payment_seed->publishable_key)) {
-					$this->element_data['stripe_public_key'] = $payment_seed->publishable_key;
+			if (isset($settings_request->response['payload']['stripe_default'])) {
+				if ($settings_request->response['payload']['stripe_default']) {
+					$payment_seed = new StripeSeed($this->element_data['user_id'],$settings_request->response['payload']['stripe_default']);
+					if (!empty($payment_seed->publishable_key)) {
+						$this->element_data['stripe_public_key'] = $payment_seed->publishable_key;
+					}
 				}
 			}
 		} else {
@@ -197,71 +216,8 @@ class SinglePurchase extends ElementBase {
 				//$this->element_data['error_message'] = $this->options['message_error'];
 				$this->element_data['error_message'] = print_r($this->original_response,true);
 			}
-		} elseif (isset($_REQUEST['initiate_checkout'])) {
-
-			// we've submitted the default stage, so we need to check if we're going to shipping stage next, or skipping to init payment stage
-			$total_price = $item['price'];
-			if (isset($_REQUEST['total_price'])) {
-				$total_price = $_REQUEST['total_price'];
-			}
-			$this->element_data['total_price'] = $total_price;
-
-			if ($item['physical_fulfillment'] == 1) {
-
-				// according to the item, we've got a product with physical fulfillment--- let's assume it's shippable
-				// and revert to no shipping if it doesn't meet the basic requirements on shipping regions
-				$this->element_data['no_shipping'] = false;
-
-				/** LEGACY NOTICE: shipping has moved from elements to items. */
-				if ($item['shipping']) {
-					// check if item has shipping first (new)
-					if ($item['shipping']['r1-1'] + $item['shipping']['r2-1'] == 0.00) {
-						$this->element_data['no_shipping'] = true;
-					} else {
-						// we've got shipping set via the new item standard, so let's give them precedence over the legacy values
-						$this->element_data['region1_cost'] = $item['shipping']['r1-1'];
-						$this->element_data['region2_cost'] = $item['shipping']['r2-1'];
-					}
-				}
-				else {
-					// else check if element has shipping (legacy)
-					if ($this->element_data['region1_cost'] + $this->element_data['region2_cost'] == 0.00) {
-						$this->element_data['no_shipping'] = true;
-					}
-				}
-			} else {
-				$this->element_data['no_shipping'] = true;
-			}
-
-			if ($total_price < $item['price']) {
-				// okay, someone's a wiseguy and trying to change the price on the checkout form
-				$this->element_data['error_message'] = 'Make sure you enter a price of at least ' . $this->element_data['currency'] . $item['price'] . ' and try again.';
-			}
-			else {
-				$this->setTemplate('checkout');
-			}
 		}
 
-		elseif (isset($_REQUEST['get_shipping'])) {
-			// we need to save values for access in init_payment
-			$request = new CASHRequest();
-			$request->sessionSet("order_data", json_encode(
-				array(
-					"shipping" => $_REQUEST['shipping'],
-					"first_name" => $_REQUEST['first_name'],
-					"last_name" => $_REQUEST['last_name'],
-					"street_address" => $_REQUEST['street_address'],
-					"street_address2" => $_REQUEST['street_address2'],
-					"city" => $_REQUEST['city'],
-					"province" => $_REQUEST['province'],
-					"postal_code" => $_REQUEST['postal_code'],
-					"country"	  => $_REQUEST['country']
-				)
-			));
-
-			$this->element_data['total_price'] = $_REQUEST['total_price'];
-			$this->setTemplate('init_payment');
-		}
 		return $this->element_data;
 	}
 } // END class
