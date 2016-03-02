b39a6fde269c8dbae34bdac2d1c636fa23917d38
Jesse von Doom <jesse@cashmusic.org>
2016-03-02 13:02:15 -0800

    header all the things!

--- interfaces/api/classes/APICore.php
+++ interfaces/api/classes/APICore.php
@@ -95,7 +95,8 @@
 		header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
 		header("Access-Control-Allow-Origin: *");
 		header('Access-Control-Allow-Credentials: true');
-		header('Access-Control-Allow-Headers: *');
+		header('Access-Control-Allow-Headers: X-Requested-With, Content-Type');
+      header('Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT');
 
 		// check the parsed_url to see if things are good
 		if (count($parsed_url['parsed'])) {
