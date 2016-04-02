e8bf1065dc3ebb6764d84b1a1b4137403c2fb2da
Jesse von Doom <jesse@cashmusic.org>
2016-04-01 17:11:28 -0700

    stripped beta flags from stuff

--- interfaces/admin/classes/AdminHelper.php
+++ interfaces/admin/classes/AdminHelper.php
@@ -203,7 +203,7 @@
 			}
 		}
 		if ($value['type'] == 'select') {
-			$default_val = AdminHelper::echoFormOptions($value['values'],0,false,true,true);
+			$default_val = AdminHelper::echoFormOptions($value['values'],0,false,true,false);
 		}
 		if ($value['type'] == 'scalar') {
 			$default_val = array();
@@ -226,7 +226,7 @@
 	public static function formatElementValue($value,$type,$formatting_data=false) {
 		$return_val = $value;
 		if ($type == 'select') {
-			$return_val = AdminHelper::echoFormOptions($formatting_data,$value,false,true,true);
+			$return_val = AdminHelper::echoFormOptions($formatting_data,$value,false,true,false);
 		}
 		return $return_val;
 	}
