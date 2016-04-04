d3924ecabab934811d7292e629da35ed9331bb20
Jesse von Doom <jesse@cashmusic.org>
2016-04-04 15:23:48 -0700

    hiding send to all buyers until we sort the mandrill junks

--- interfaces/admin/components/pages/controllers/commerce_items_send.php
+++ interfaces/admin/components/pages/controllers/commerce_items_send.php
@@ -2,7 +2,5 @@
 $cash_admin->page_data['ui_title'] = '';
 $cash_admin->page_data['id'] = $request_parameters[0];
 
-$cash_admin->page_data['connection_options'] = AdminHelper::echoConnectionsOptions('mass_email',0,true);
-
 $cash_admin->setPageContentTemplate('commerce_items_send');
 ?>
