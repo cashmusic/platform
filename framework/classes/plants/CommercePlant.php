1018bc384512c451d17ff1a2508d969dea59a3b3
Jesse von Doom <jesse@cashmusic.org>
2016-04-05 11:22:37 -0700

    progress on parseTransaction

--- framework/classes/plants/CommercePlant.php
+++ framework/classes/plants/CommercePlant.php
@@ -898,83 +898,137 @@ class CommercePlant extends PlantBase {
     protected function parseTransactionData($data_returned,$data_sent) {
       if (!is_array($data_returned)) {
           $data_returned = json_decode($data_returned,true);
-
-          if (isset($data_returned['payer'])) {
-              $data_returned['customer_email'] = $data_returned['payer']['email'];
-              $data_returned['customer_first_name'] = $data_returned['payer']['first_name'];
-              $data_returned['customer_last_name'] = $data_returned['payer']['last_name'];
-              $data_returned['customer_name'] = $data_returned['payer']['first_name'] . " " . $data_returned['payer']['last_name'];
-          }
-
       }
       if (!is_array($data_sent)) {
          $data_sent = json_decode($data_sent,true);
       }
-
-      if (isset($data_returned['customer_name']) && isset($data_returned['total'])) {
-         return $data_returned;
-      } else {
-         if (isset($data_sent['PAYMENTREQUEST_0_DESC'])) {
-            $return_array = array(
-   				'transaction_description' => $data_sent['PAYMENTREQUEST_0_DESC'],
-   				'customer_email' => $data_sent['EMAIL'],
-   				'customer_first_name' => $data_sent['FIRSTNAME'],
-   				'customer_last_name' => $data_sent['LASTNAME'],
-   				'customer_name' => $data_sent['FIRSTNAME'] . ' ' . $data_sent['LASTNAME']
-   			);
-   			// this is ugly, but the if statements normalize Paypal's love of omitting empty data
-   			if (isset($data_sent['PAYMENTREQUEST_0_SHIPTONAME'])) {
-   				$return_array['customer_shipping_name'] = $data_sent['PAYMENTREQUEST_0_SHIPTONAME'];
-   			} else {
-   				$return_array['customer_shipping_name'] = '';
-   			}
-
-
-   			if (isset($data_sent['PAYMENTREQUEST_0_SHIPTOSTREET'])) {
-   				$return_array['customer_address1'] = $data_sent['PAYMENTREQUEST_0_SHIPTOSTREET'];
-   			} else {
-   				$return_array['customer_address1'] = '';
-   			}
-   			if (isset($data_sent['PAYMENTREQUEST_0_SHIPTOSTREET2'])) {
-   				$return_array['customer_address2'] = $data_sent['PAYMENTREQUEST_0_SHIPTOSTREET2'];
-   			} else {
-   				$return_array['customer_address2'] = '';
-   			}
-   			if (isset($data_sent['PAYMENTREQUEST_0_SHIPTOCITY'])) {
-   				$return_array['customer_city'] = $data_sent['PAYMENTREQUEST_0_SHIPTOCITY'];
-   			} else {
-   				$return_array['customer_city'] = '';
-   			}
-   			if (isset($data_sent['PAYMENTREQUEST_0_SHIPTOSTATE'])) {
-   				$return_array['customer_region'] = $data_sent['PAYMENTREQUEST_0_SHIPTOSTATE'];
-   			} else {
-   				$return_array['customer_region'] = '';
-   			}
-   			if (isset($data_sent['PAYMENTREQUEST_0_SHIPTOZIP'])) {
-   				$return_array['customer_postalcode'] = $data_sent['PAYMENTREQUEST_0_SHIPTOZIP'];
-   			} else {
-   				$return_array['customer_postalcode'] = '';
-   			}
-   			if (isset($data_sent['SHIPTOCOUNTRYNAME'])) {
-   				$return_array['customer_country'] = $data_sent['SHIPTOCOUNTRYNAME'];
-   			} else {
-   				$return_array['customer_country'] = '';
-   			}
-   			if (isset($data_sent['PAYMENTREQUEST_0_SHIPTOCOUNTRYCODE'])) {
-   				$return_array['customer_countrycode'] = $data_sent['PAYMENTREQUEST_0_SHIPTOCOUNTRYCODE'];
-   			} else {
-   				$return_array['customer_countrycode'] = '';
-   			}
-   			if (isset($data_sent['PAYMENTREQUEST_0_SHIPTOPHONENUM'])) {
-   				$return_array['customer_phone'] = $data_sent['PAYMENTREQUEST_0_SHIPTOPHONENUM'];
-   			} else {
-   				$return_array['customer_phone'] = '';
-   			}
-   			return $return_array;
-         } else {
-            return false;
+      if (is_array($data_returned)) {
+         if (isset($data_returned['payer'])) {
+             $data_returned['customer_email'] = $data_returned['payer']['email'];
+             $data_returned['customer_first_name'] = $data_returned['payer']['first_name'];
+             $data_returned['customer_last_name'] = $data_returned['payer']['last_name'];
+             $data_returned['customer_name'] = $data_returned['payer']['first_name'] . " " . $data_returned['payer']['last_name'];
+             if (isset($data_returned['payer']['last_name'])) {
+                $return_array['customer_shipping_name'] = $data_sent['PAYMENTREQUEST_0_SHIPTONAME'];
+             } else {
+                $return_array['customer_shipping_name'] = '';
+             }
+             if (isset($data_returned['payer']['last_name'])) {
+                $return_array['customer_address1'] = $data_sent['PAYMENTREQUEST_0_SHIPTOSTREET'];
+             } else {
+                $return_array['customer_address1'] = '';
+             }
+             if (isset($data_returned['payer']['last_name'])) {
+                $return_array['customer_address2'] = $data_sent['PAYMENTREQUEST_0_SHIPTOSTREET2'];
+             } else {
+                $return_array['customer_address2'] = '';
+             }
+             if (isset($data_returned['payer']['last_name'])) {
+                $return_array['customer_city'] = $data_sent['PAYMENTREQUEST_0_SHIPTOCITY'];
+             } else {
+                $return_array['customer_city'] = '';
+             }
+             if (isset($data_returned['payer']['last_name'])) {
+                $return_array['customer_region'] = $data_sent['PAYMENTREQUEST_0_SHIPTOSTATE'];
+             } else {
+                $return_array['customer_region'] = '';
+             }
+             if (isset($data_returned['payer']['last_name'])) {
+                $return_array['customer_postalcode'] = $data_sent['PAYMENTREQUEST_0_SHIPTOZIP'];
+             } else {
+                $return_array['customer_postalcode'] = '';
+             }
+             if (isset($data_returned['payer']['last_name'])) {
+                $return_array['customer_country'] = $data_sent['SHIPTOCOUNTRYNAME'];
+             } else {
+                $return_array['customer_country'] = '';
+             }
+             if (isset($data_returned['payer']['country_code'])) {
+                $return_array['customer_countrycode'] = $data_returned['payer']['country_code'];
+             } else {
+                $return_array['customer_countrycode'] = '';
+             }
+             if (isset($data_returned['payer']['phone'])) {
+                $return_array['customer_phone'] = $data_returned['payer']['phone'];
+             } else {
+                $return_array['customer_phone'] = '';
+             }
          }
       }
+
+      if (is_array($data_returned)) {
+         if (isset($data_returned['customer_name']) && isset($data_returned['total'])) {
+            return $data_returned;
+         }
+      }
+
+      // LEGACY TRANSACTION
+      if (is_array($data_sent)) {
+         if (isset($data_sent['PAYMENTREQUEST_0_DESC'])) {
+            if (isset($data_sent['PAYMENTREQUEST_0_DESC'])) {
+               $return_array = array(
+      				'transaction_description' => $data_sent['PAYMENTREQUEST_0_DESC'],
+      				'customer_email' => $data_sent['EMAIL'],
+      				'customer_first_name' => $data_sent['FIRSTNAME'],
+      				'customer_last_name' => $data_sent['LASTNAME'],
+      				'customer_name' => $data_sent['FIRSTNAME'] . ' ' . $data_sent['LASTNAME']
+      			);
+      			// this is ugly, but the if statements normalize Paypal's love of omitting empty data
+      			if (isset($data_sent['PAYMENTREQUEST_0_SHIPTONAME'])) {
+      				$return_array['customer_shipping_name'] = $data_sent['PAYMENTREQUEST_0_SHIPTONAME'];
+      			} else {
+      				$return_array['customer_shipping_name'] = '';
+      			}
+
+
+      			if (isset($data_sent['PAYMENTREQUEST_0_SHIPTOSTREET'])) {
+      				$return_array['customer_address1'] = $data_sent['PAYMENTREQUEST_0_SHIPTOSTREET'];
+      			} else {
+      				$return_array['customer_address1'] = '';
+      			}
+      			if (isset($data_sent['PAYMENTREQUEST_0_SHIPTOSTREET2'])) {
+      				$return_array['customer_address2'] = $data_sent['PAYMENTREQUEST_0_SHIPTOSTREET2'];
+      			} else {
+      				$return_array['customer_address2'] = '';
+      			}
+      			if (isset($data_sent['PAYMENTREQUEST_0_SHIPTOCITY'])) {
+      				$return_array['customer_city'] = $data_sent['PAYMENTREQUEST_0_SHIPTOCITY'];
+      			} else {
+      				$return_array['customer_city'] = '';
+      			}
+      			if (isset($data_sent['PAYMENTREQUEST_0_SHIPTOSTATE'])) {
+      				$return_array['customer_region'] = $data_sent['PAYMENTREQUEST_0_SHIPTOSTATE'];
+      			} else {
+      				$return_array['customer_region'] = '';
+      			}
+      			if (isset($data_sent['PAYMENTREQUEST_0_SHIPTOZIP'])) {
+      				$return_array['customer_postalcode'] = $data_sent['PAYMENTREQUEST_0_SHIPTOZIP'];
+      			} else {
+      				$return_array['customer_postalcode'] = '';
+      			}
+      			if (isset($data_sent['SHIPTOCOUNTRYNAME'])) {
+      				$return_array['customer_country'] = $data_sent['SHIPTOCOUNTRYNAME'];
+      			} else {
+      				$return_array['customer_country'] = '';
+      			}
+      			if (isset($data_sent['PAYMENTREQUEST_0_SHIPTOCOUNTRYCODE'])) {
+      				$return_array['customer_countrycode'] = $data_sent['PAYMENTREQUEST_0_SHIPTOCOUNTRYCODE'];
+      			} else {
+      				$return_array['customer_countrycode'] = '';
+      			}
+      			if (isset($data_sent['PAYMENTREQUEST_0_SHIPTOPHONENUM'])) {
+      				$return_array['customer_phone'] = $data_sent['PAYMENTREQUEST_0_SHIPTOPHONENUM'];
+      			} else {
+      				$return_array['customer_phone'] = '';
+      			}
+               return $return_array;
+            } else {
+               return false;
+            }
+         }
+      }
+
+      return false;
     }
 
     protected function getOrdersForUser($user_id,$include_abandoned=false,$max_returned=false,$since_date=0,$unfulfilled_only=0,$deep=false,$skip=0) {
@@ -1605,7 +1659,7 @@ class CommercePlant extends PlantBase {
 
         // call the payment seed class
         $payment_seed = new $seed_class($order_details['user_id'],$transaction_details['connection_id']);
-        
+
         // if this was approved by the user, we need to compare some values to make sure everything matches up
         if ($payment_details = $payment_seed->doPayment($total_price, $description, $token, $email_address, $customer_name, $shipping_info, $subtotal, $session_id)) {
             // okay, we've got the matching totals, so let's get the $user_id, y'all
