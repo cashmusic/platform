ac02128a9d7bd9ed97a38725ae8e67af647096ff
Jesse von Doom <jesse@cashmusic.org>
2016-04-26 13:47:11 -0700

    elements and whatnot.

--- interfaces/admin/components/pages/controllers/campaigns_delete.php
+++ interfaces/admin/components/pages/controllers/campaigns_delete.php
@@ -6,16 +6,16 @@ if (!$request_parameters) {
 if (isset($_POST['dodelete']) || isset($_REQUEST['modalconfirm'])) {
 	$delete_response = $cash_admin->requestAndStore(
 		array(
-			'cash_request_type' => 'element', 
+			'cash_request_type' => 'element',
 			'cash_action' => 'deletecampaign',
 			'id' => $request_parameters[0]
 		)
 	);
 	if ($delete_response['status_uid'] == 'element_deletecampaign_200') {
-		// get all campaigns 
+		// get all campaigns
 		$campaigns_response = $cash_admin->requestAndStore(
 			array(
-				'cash_request_type' => 'element', 
+				'cash_request_type' => 'element',
 				'cash_action' => 'getcampaignsforuser',
 				'user_id' => $cash_admin->effective_user_id
 			)
@@ -24,13 +24,23 @@ if (isset($_POST['dodelete']) || isset($_REQUEST['modalconfirm'])) {
 		if (count($campaigns_response['payload'])) {
 			$current_campaign = $campaigns_response['payload'][count($campaigns_response['payload']) - 1]['id'];
 			$admin_primary_cash_request->sessionSet('current_campaign',$current_campaign);
-		
+
 			$settings_request = new CASHRequest(
 				array(
-					'cash_request_type' => 'system', 
+					'cash_request_type' => 'system',
 					'cash_action' => 'setsettings',
 					'type' => 'selected_campaign',
 					'value' => $current_campaign,
+					'user_id' => $cash_admin->effective_user_id
+				)
+			);
+		} else {
+			$settings_request = new CASHRequest(
+				array(
+					'cash_request_type' => 'system',
+					'cash_action' => 'setsettings',
+					'type' => 'selected_campaign',
+					'value' => -1,
 					'user_id' => $cash_admin->effective_user_id
 				)
 			);
