b0c7df04c647b4645f1fa9fa512308983e131c9e
Chris Leckie <me@christopherleckie.com>
2016-07-22 15:43:54 -0700

    2016

--- framework/elements/TourDates.php
+++ framework/elements/TourDates.php
@@ -6,7 +6,7 @@
  * @author CASH Music
  * @link http://cashmusic.org/
  *
- * Copyright (c) 2013, CASH Music
+ * Copyright (c) 2016, CASH Music
  * Licensed under the Affero General Public License version 3.
  * See http://www.gnu.org/licenses/agpl-3.0.html
  *
