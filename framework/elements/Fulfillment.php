8b2874706fe214ed34834ff2dfef25fac1d3f9bf
Jesse von Doom <jesse@cashmusic.org>
2016-08-22 15:18:22 -0700

    basic working frame for fulfillment element

--- framework/elements/Fulfillment.php
+++ framework/elements/Fulfillment.php
@@ -10,37 +10,26 @@
  * Licensed under the Affero General Public License version 3.
  * See http://www.gnu.org/licenses/agpl-3.0.html
  *
- *
- * This file is generously sponsored by Anant Narayanan [anant@kix.in]
- * define FALSE TRUE — Just kidding.
- *
  **/
-class DownloadCodes extends ElementBase {
-	public $type = 'downloadcodes';
-	public $name = 'Download Codes';
+class Fulfillment extends ElementBase {
+	public $type = 'fulfillment';
+	public $name = '3rd Party Fulfillment';
 
 	public function getData() {
+		$this->element_data['element_id'] = $this->element_id;
+		$this->element_data['public_url'] = CASH_PUBLIC_URL;
+
 		if ($this->status_uid == 'asset_redeemcode_400') {
 			$this->element_data['error_message'] = 'That code is not valid or has already been used.';
-		} elseif ($this->status_uid == 'asset_redeemcode_200') {
-			// first we "unlock" the asset, telling the platform it's okay to generate a link for non-private assets
-			$this->element_data['asset_id'] = $this->original_response['payload']['scope_table_id'];
-			if ($this->element_data['asset_id'] != 0) {
-				// get all fulfillment assets
-				$fulfillment_request = new CASHRequest(
-					array(
-						'cash_request_type' => 'asset', 
-						'cash_action' => 'getfulfillmentassets',
-						'asset_details' => $this->element_data['asset_id']
-					)
-				);
-				if ($fulfillment_request->response['payload']) {
-					$this->element_data['fulfillment_assets'] = new ArrayIterator($fulfillment_request->response['payload']);
-				}
-			}
-			$this->setTemplate('success');
+		} elseif (isset($_REQUEST['code'])) {
+			$this->element_data['code'] = $_REQUEST['code'];
+			//$this->setTemplate('success');
+		} elseif (isset($_REQUEST['processcode'])) {
+			$this->element_data['code'] = $_REQUEST['processcode'];
+			$this->setTemplate('location');
 		}
+
 		return $this->element_data;
 	}
-} // END class 
+} // END class
 ?>
