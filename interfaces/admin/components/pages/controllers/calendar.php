9e4bdd6061e542677a8c6fa680e664d1836d1b77
Chris Leckie <me@christopherleckie.com>
2016-10-11 14:39:42 -0700

    shimmy shimmy shake

--- interfaces/admin/components/pages/controllers/calendar.php
+++ interfaces/admin/components/pages/controllers/calendar.php
@@ -61,6 +61,51 @@ $allevents_response = $cash_admin->requestAndStore(
 	)
 );
 
+$event = "";
+// Archive events
+$allpast_response = $cash_admin->requestAndStore(
+	array(
+		'cash_request_type' => 'calendar',
+		'cash_action' => 'getevents',
+		'user_id' => $cash_admin->effective_user_id,
+        'published_status' => 1,
+		'visible_event_types' => 'archive'
+	)
+);
+
+if (is_array($allpast_response['payload'])) {
+    formatEventOutput($allpast_response);
+
+    $cash_admin->page_data['events_allpast'] = new ArrayIterator(array_reverse($allpast_response['payload']));
+}
+
+
+// Upcoming events
+$allfuture_response = $cash_admin->requestAndStore(
+	array(
+		'cash_request_type' => 'calendar',
+		'cash_action' => 'getevents',
+		'user_id' => $cash_admin->effective_user_id,
+        'published_status' => 1,
+		'visible_event_types' => 'upcoming'
+	)
+);
+
+
+if (is_array($allfuture_response['payload'])) {
+
+    formatEventOutput($allfuture_response);
+
+	$cash_admin->page_data['events_allfuture'] = new ArrayIterator($allfuture_response['payload']);
+}
+
+
+$cash_admin->page_data['options_venues'] = AdminHelper::echoFormOptions('venues',0,false,true);
+
+//Is Event Published Page data
+$cash_admin->page_data['published'] = $event['published'];
+
+
 $cash_admin->page_data['no_events'] = !$allevents_response['payload'];
 
 
