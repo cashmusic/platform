ad765e5d25d9f1281548b6353d789fb698c476d9
paperscissors <tom@paperscissorsandglue.com>
2016-11-04 15:36:07 -0700

    plan details edit

---
+++ interfaces/admin/components/pages/controllers/commerce_subscriptions_edit.php
@@ -0,0 +1,31 @@
+<?php
+    
+    $settings_request = new CASHRequest(
+        array(
+            'cash_request_type' => 'system',
+            'cash_action' => 'getsettings',
+            'type' => 'payment_defaults',
+            'user_id' => $cash_admin->effective_user_id
+        )
+    );
+    if (is_array($settings_request->response['payload'])) {
+        $stripe_default = (isset($settings_request->response['payload']['stripe_default'])) ? $settings_request->response['payload']['stripe_default'] : false;
+    }
+
+// plan index
+    $subscription_request = new CASHRequest(
+        array(
+            'cash_request_type' => 'commerce',
+            'cash_action' => 'getsubscriptionplan',
+            'user_id' => $cash_admin->effective_user_id,
+            'id' => $request_parameters[0]
+        )
+    );
+
+    if ($subscription_request->response['payload']) {
+
+        $cash_admin->page_data['plan'] = $subscription_request->response['payload'];
+    }
+
+    $cash_admin->setPageContentTemplate('commerce_subscriptions_edit');
+    ?>
