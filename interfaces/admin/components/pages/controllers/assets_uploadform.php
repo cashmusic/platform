2cf8adb8295176f24e89d039c674fbef9640559b
tomfilepp <tom@paperscissorsandglue.com>
2017-01-06 12:26:07 -0800

    successful CORS upload + progress bar

--- interfaces/admin/components/pages/controllers/assets_uploadform.php
+++ interfaces/admin/components/pages/controllers/assets_uploadform.php
@@ -9,10 +9,13 @@ if (isset($request_parameters[0])) {
 			'user_id' => $cash_admin->effective_user_id
 		)
 	);
+
+
 	if (is_array($param_response['payload'])) {
 		$cash_admin->page_data = array_merge($cash_admin->page_data,$param_response['payload']);
 		$cash_admin->page_data['connection_id'] = $request_parameters[0];
 		$which_form = str_replace('.','',$param_response['payload']['connection_type']);
+
 		$cash_admin->setPageContentTemplate('assets_uploadform_' . $which_form);
 	}
 }
