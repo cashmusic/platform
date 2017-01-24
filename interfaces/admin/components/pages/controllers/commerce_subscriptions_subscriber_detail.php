a84b887093788568d459b62384404e612e81c36f
tomfilepp <tom@paperscissorsandglue.com>
2017-01-24 15:11:20 -0800

    formatted payment timestamp

--- interfaces/admin/components/pages/controllers/commerce_subscriptions_subscriber_detail.php
+++ interfaces/admin/components/pages/controllers/commerce_subscriptions_subscriber_detail.php
@@ -94,7 +94,7 @@ if (is_array($settings_request->response['payload'])) {
 
             foreach ($payments as $payment) {
 
-                $payment['service_timestamp'] = date('m/d/Y g:i:s A', $payment['service_timestamp']);
+                $payment['service_timestamp'] = date('m/d/Y g:i A', $payment['service_timestamp']);
                 $cash_admin->page_data['subscriptions_payment'][] = $payment;
             }
         }
