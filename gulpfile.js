2370c52093ea154c36d1bbe47dba4953e664d8d4
tomfilepp <tom@paperscissorsandglue.com>
2017-02-02 12:04:28 -0800

    gulp json lint watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
 var gulp = require('gulp');
 var closure = require('gulp-closure-compiler-service');
+var jsonlint = require("gulp-jsonlint");
 
-var source_path = './interfaces/public/cashmusicjs/source/';
+var js_source = './interfaces/public/cashmusicjs/source/';
 var paths = {
   scripts: [
-      source_path+'cashmusic.js',
-      source_path+'checkout/checkout.js']
+      js_source+'cashmusic.js',
+      js_source+'checkout/checkout.js'],
+    json: [
+        './framework/settings/**/*.json',
+        './framework/elements/**/*.json',
+        './interfaces/admin/components/**/*.json'
+    ]
 };
 
 gulp.task('compile', function() {
@@ -14,8 +20,15 @@ gulp.task('compile', function() {
         .pipe(gulp.dest('interfaces/public/'));
 });
 
+gulp.task('jsonlint', function() {
+    return gulp.src(paths.json)
+        .pipe(jsonlint())
+        .pipe(jsonlint.reporter());
+});
+
 gulp.task('watch', function() {
     gulp.watch(paths.scripts, ['compile']);
+    gulp.watch(paths.json, ['jsonlint']);
 });
 
 gulp.task('default', ['compile']);
