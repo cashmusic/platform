c4bcc97324197990ad7d384fbe61e839eeebc0d8
tomfilepp <tom@paperscissorsandglue.com>
2017-02-15 11:27:21 -0800

    share button library works gulp rsync watcher

--- interfaces/public/cashmusicjs/source/share/share-buttons.js
+++ interfaces/public/cashmusicjs/source/share/share-buttons.js
@@ -1,19 +1,19 @@
 (function() {
+    'use strict';
+    var cm = window.cashmusic;
 
-    window.cashmusic.share_buttons = new function () {
-        this.element = null;
-        this.buttons = "";
+    cm.shareButtons = {
 
-        this.template = function (type, url) {
+        template: function (type, url) {
             var type = type.toLowerCase().trim();
             return "<a target='blank' class='cm-share-button cm-share-button-" + type + "' href='" + url + "'>" + type + "</a>";
-        };
+        },
 
-        this.buildButtons = function (element, options) {
+        buildButtons: function (element, options) {
 
-            this.element = document.querySelector(element);
+            var element = document.querySelector(element);
 
-            this.buttons += this.template("facebook", "https://www.facebook.com/dialog/feed?app_id=440889266256683&link="
+            var buttons = this.template("facebook", "https://www.facebook.com/dialog/feed?app_id=440889266256683&link="
                 + encodeURIComponent(options.url)
                 + "&name=" + encodeURIComponent(options.title)
                 + "&caption=" + encodeURIComponent(options.caption)
@@ -21,27 +21,27 @@
                 + "&picture=" + encodeURIComponent(options.preview_image_url)
             );
 
-            this.buttons += this.template("tumblr",
+            buttons += this.template("tumblr",
                 "http://www.tumblr.com/share/link?url="
                 + encodeURIComponent(options.url)
                 + "&name=" + encodeURIComponent(options.title)
                 + "&caption=" + encodeURIComponent(options.description)
             );
 
-            this.buttons += this.template("twitter",
+            buttons += this.template("twitter",
                 "https://twitter.com/intent/tweet?text="
                 + encodeURIComponent(options.title)
                 + "&url=" + encodeURIComponent(options.url)
             );
 
-            this.buttons += this.template("reddit",
+            buttons += this.template("reddit",
                 "http://www.reddit.com/submit?url="
                 + encodeURIComponent(options.url)
                 + "&title=" + encodeURIComponent(options.title)
             );
 
-            this.element.innerHTML = this.element.innerHTML + this.buttons;
+            element.innerHTML = element.innerHTML + buttons;
 
-        };
+        }
     };
-});
+}());
