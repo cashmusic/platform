d96794596de6c879703ca1406c475c805463fe35
tomfilepp <tom@paperscissorsandglue.com>
2017-02-17 12:55:53 -0800

    doc blocks

--- framework/elements/Interfaces/DataInterface.php
+++ framework/elements/Interfaces/DataInterface.php
@@ -4,6 +4,12 @@ namespace Cashmusic\Elements\Interfaces;
 
 interface DataInterface
 {
+    /**
+     * Do whatever you want to do here, so long as it returns an array of key=>values that
+     * you want to be merged into element_data.
+     *
+     * @return array
+     */
     public function getData();
 }
 
--- framework/elements/Interfaces/StatesInterface.php
+++ framework/elements/Interfaces/StatesInterface.php
@@ -4,6 +4,18 @@ namespace Cashmusic\Elements\Interfaces;
 
 interface StatesInterface
 {
+    /**
+     * State router. Ideally this will have a switch/case based on $_REQUEST['state'] that
+     * returns an array with template and data. Data is merged into the element_data array.
+     *
+     * [
+     * 'template' => 'default',
+     * 'data' => [...]
+     * ]
+     *
+     * @param $callback
+     * @return array
+     */
     public function router($callback);
 }
 
