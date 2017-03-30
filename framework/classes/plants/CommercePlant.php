1998c4ed720056fda6980276d078a3aaa3ae864e
tomfilepp <tom@paperscissorsandglue.com>
2017-03-30 13:54:43 -0700

    live family endpoints for comps

--- framework/classes/plants/CommercePlant.php
+++ framework/classes/plants/CommercePlant.php
@@ -2937,6 +2937,9 @@ class CommercePlant extends PlantBase {
             )
         );
 
+        error_log(".d.d.d.d.d.d $email .. $password".print_r($validate_request, true));
+        error_log(".d.d.d.d " .print_r($plans, true));
+
         // email or password are not set so bail, or they're set but they don't validate
         if ( (!$email || !$password || !$plans) || !$validate_request->response['payload'] ) {
             return "401";
