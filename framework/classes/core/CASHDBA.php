fd2845ab4099a501a98bd81e8012cf18a7434db6
tomfilepp <tom@paperscissorsandglue.com>
2017-04-03 14:17:52 -0700

    limit on sub totals

--- framework/classes/core/CASHDBA.php
+++ framework/classes/core/CASHDBA.php
@@ -639,7 +639,7 @@ class CASHDBA {
 				$query = "select sum(gross_price) as total_active from commerce_transactions as t "
 						 ."join commerce_subscriptions_members as m on t.parent_id = m.id AND t.parent = 'sub' and t.status = 'success' "
 				         ."WHERE m.status = 'active' AND m.subscription_id = :plan_id "
-						 ."ORDER BY creation_date DESC LIMIT 1";
+						 ."ORDER BY t.creation_date DESC LIMIT 1";
 
 				break;
 
