59d460ef97f2c3efbc9f41f0501c91adc589e55d
tomfilepp <tom@paperscissorsandglue.com>
2017-04-04 17:36:15 -0700

    full namespace enchiladas changed class loader, got rid of require based class loaded on admin

--- framework/classes/core/CASHRequest.php
+++ framework/classes/core/CASHRequest.php
@@ -92,9 +92,7 @@ use CASHMusic\Core\CASHDaemon as CASHDaemon;
 			if ($requested_plant != '' && count($this->request) > 0) {
 				$this->buildPlantArray();
 				if (isset($this->plant_array[$requested_plant])) {
-					//$file_path = CASH_PLATFORM_ROOT.'/classes/plants/'.$this->plant_array[$requested_plant];
 					$class_name = $namespace.substr_replace($this->plant_array[$requested_plant], '', -4);
-					//require_once($file_path);
 					$this->plant = new $class_name($this->request_method,$this->request);
 					$this->response = $this->plant->processRequest();
 				}
