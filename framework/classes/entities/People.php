73f40565ad421fe784d352173f4d0abd8b4bab50
tomfilepp <tom@paperscissorsandglue.com>
2017-05-27 18:12:53 -0700

    removed IDE property docs for brevity

--- framework/classes/entities/People.php
+++ framework/classes/entities/People.php
@@ -3,31 +3,7 @@
 namespace CASHMusic\Entities;
 
 /**
- * Class User
- * @package CASHMusic\Entities
  * @Entity @Table(name="people")
- *
- * @property integer $id
- * @property string $username
- * @property string $email_address
- * @property string $display_name
- * @property string $first_name
- * @property string $last_name
- * @property string $organization
- * @property string $address_line1
- * @property string $address_line2
- * @property string $address_city
- * @property string $address_region
- * @property string $address_postalcode
- * @property string $address_country
- * @property string $url
- * @property string $is_admin
- * @property string $api_key
- * @property string $api_secret
- * @property string $creation_date
- * @property string $modification_date
- * @property string $password
- * @property array $data
  */
 
 class People extends EntityBase {
