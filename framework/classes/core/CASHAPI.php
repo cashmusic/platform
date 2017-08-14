ba0a1d1868ce10b57b2cd2ae443e8f6e678a6f38
tomfilepp <tom@paperscissorsandglue.com>
2017-08-14 10:09:26 -0700

    options

--- framework/classes/core/CASHAPI.php
+++ framework/classes/core/CASHAPI.php
@@ -37,7 +37,6 @@ class CASHAPI
 
     public function __construct()
     {
-        CASHSystem::dd($_REQUEST);
         CASHSystem::startUp(false);
 
         list($accessTokenRepository, $server, $resourceServer) = $this->getAuthorizationServer();
@@ -59,10 +58,10 @@ class CASHAPI
         });*/
 
 
-        $api->get('/verbose/{plant}/{noun}[/{arg1}/{arg1_val}/{arg2}/{arg2_val}]', function ($request, $response, $args) use ($api) {
+        $api->get('/verbose/{plant}/{noun}[/{arg1}/{arg1_val}/{arg2}/{arg2_val}/]', function ($request, $response, $args) use ($api) {
 
             CASHSystem::dd($args);
-            $api->redirect('/new', 301);
+            //$api->redirect('/new', 301);
         });
 
 
