c491779983ea6f58ca219e736e87f05f0cf3f8d9
tomfilepp <tom@paperscissorsandglue.com>
2017-08-18 10:36:05 -0700

    basic logout state

--- interfaces/public/cashmusicjs/source/cashmusic.js
+++ interfaces/public/cashmusicjs/source/cashmusic.js
@@ -12,7 +12,7 @@
  * Copyright (c) 2016, CASH Music
  * All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without modification,
+ * Redstribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
  *
  * Redistributions of source code must retain the above copyright notice, this list
