7ce9be5e119318790b920b8c019993a54810ff04
tomfilepp <tom@paperscissorsandglue.com>
2017-09-01 17:59:20 -0700

    give ability for at least global element CSS load if exists

--- interfaces/public/request/request.php
+++ interfaces/public/request/request.php
@@ -22,12 +22,11 @@ header('Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT');
 if (isset($_REQUEST['element_style'])) {
     header("Content-Type: text/css");
     header("X-Content-Type-Options: nosniff");
-    $root = realpath(dirname(__FILE__) . '/../..');
-    if (!defined('CASH_PLATFORM_ROOT')) define('CASH_PLATFORM_ROOT', $root);
+    $root = realpath(dirname(__FILE__) . '/../../../framework');
 
-    echo CASH_PLATFORM_ROOT . $_REQUEST['element_style'];
-    if (file_exists(CASH_PLATFORM_ROOT . $_REQUEST['element_style']))
-    echo CASHSystem::getFileContents(CASH_PLATFORM_ROOT . $_REQUEST['element_style']);
+    echo $root . $_REQUEST['element_style'];
+    if (file_exists($root . $_REQUEST['element_style']))
+    echo CASHSystem::getFileContents($root . $_REQUEST['element_style']);
     exit;
 }
 if (!isset($_REQUEST['nooutput'])) {
