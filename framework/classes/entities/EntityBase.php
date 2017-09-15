a6733ac64f027d0c154e0ce34f4c2a16cd67d55f
tomfilepp <tom@paperscissorsandglue.com>
2017-09-15 13:26:53 -0700

    search subscriptions in admin

--- framework/classes/entities/EntityBase.php
+++ framework/classes/entities/EntityBase.php
@@ -102,9 +102,9 @@ class EntityBase extends CASHData
             $i=0;
             foreach ($values as $field=>$value) {
                 if ($i==0) {
-                    $query = $query->where('o.'.$field.' LIKE ?'.$i, $value);
+                    $query = $query->where('o.'.$field.' LIKE :v'.$i, $value);
                 } else {
-                    $query = $query->orWhere('o.'.$field.' LIKE ?'.$i, $value);
+                    $query = $query->orWhere('o.'.$field.' LIKE :v'.$i, $value);
                 }
 
             }
