da7d7f55d4060417505b319823caa7ca8c6623e9
tomfilepp <tom@paperscissorsandglue.com>
2017-09-29 10:34:42 -0700

    change limits for orders

--- interfaces/admin/components/pages/controllers/commerce.php
+++ interfaces/admin/components/pages/controllers/commerce.php
@@ -241,7 +241,7 @@ $order_request = array(
 	'cash_action' => 'getordersforuser',
 	'user_id' => $cash_admin->effective_user_id,
 	'max_returned' => 20,
-	'skip' => ($cash_admin->page_data['current_page'] - 1) * 18,
+	'skip' => ($cash_admin->page_data['current_page'] - 1) * 15,
 	'deep' => true
 );
 
