ba11bb31f7a66dbc25e4c3582e10b96d6298a834
tomfilepp <tom@paperscissorsandglue.com>
2017-10-03 10:14:54 -0700

    wrong object type for twitter seed connection

--- framework/elements/SocialFeeds/SocialFeeds.php
+++ framework/elements/SocialFeeds/SocialFeeds.php
@@ -43,8 +43,8 @@ class SocialFeeds extends ElementBase {
 		if (isset($this->options['twitter'])) {
 			if (is_array($this->options['twitter'])) {
 				foreach($this->options['twitter'] as $feedname => $feed) {
-					CASHSystem::errorLog($feedname);
-					CASHSystem::errorLog($feed);
+
+					if (!isset($feed['twitterhidereplies'])) $feed['twitterhidereplies'] = false;
 					$twitter_request = $this->twitter_seed->getUserFeed($feed['twitterusername'],$feed['twitterhidereplies'],$this->options['post_limit'],$feed['twitterfiltertype'],$feed['twitterfiltervalue']);
 					if ($twitter_request) {
 						$twitter_feeds[] = $twitter_request;
