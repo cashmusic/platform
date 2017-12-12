8d61c6ca99c6f6e5a6c305412d1376d605f1f0d5
tomfilepp <tom@paperscissorsandglue.com>
2017-12-11 16:15:06 -0800

    possible to use scalar on element setup?

--- framework/classes/plants/Element/ElementPlant.php
+++ framework/classes/plants/Element/ElementPlant.php
@@ -96,7 +96,7 @@ class ElementPlant extends PlantBase {
 			foreach ($app_json['options'] as $section_name => $details) {
 				foreach ($details['data'] as $data => $values) {
 					if (isset($values['required'])) {
-						if ($values['type'] == 'select' && $values['required'] == true) {
+						if (in_array($values['type'], ['select', 'scalar']) && $values['required'] == true) {
 							if (is_string($values['values'])) {
 								if (substr($values['values'],0,7) == 'connect') {
 									$scope = explode('/',$values['values']);
