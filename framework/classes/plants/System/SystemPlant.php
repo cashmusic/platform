1b82a7e443446efab31209d93279b3c02c75c749
tomfilepp <tom@paperscissorsandglue.com>
2017-12-13 17:02:29 -0800

    settings troubleshoot, currency

--- framework/classes/plants/System/SystemPlant.php
+++ framework/classes/plants/System/SystemPlant.php
@@ -584,7 +584,7 @@ class SystemPlant extends PlantBase {
 	protected function getSettings($user_id,$type,$return_json=false) {
 
 		$setting = $this->orm->findWhere(SystemSettings::class, ['type'=>$type,'user_id'=>$user_id] );
-
+		CASHSystem::errorLog($setting->toArray());
 		if ($setting) {
 			if ($return_json) {
 				return json_encode($setting->toArray());
